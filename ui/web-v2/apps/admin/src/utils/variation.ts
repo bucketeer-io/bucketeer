8c568977d6b22c20ca916f8e61b5bb144e1c2da9
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2022-11-09 15:28:00 +0900

    chore: display variation name in the variation label (#68)

---
+++ ui/web-v2/apps/admin/src/utils/variation.ts
@@ -0,0 +1,16 @@
+import { Variation } from '@/proto/feature/variation_pb';
+
+export const createVariationLabel = (variation: Variation.AsObject) => {
+  if (variation == null) {
+    return 'None';
+  }
+  const maxLength = 150;
+  const ellipsis = '...';
+  const label = variation.name
+    ? variation.name + ' - ' + variation.value
+    : variation.value;
+  if (label.length > maxLength) {
+    return label.slice(0, maxLength - ellipsis.length) + ellipsis;
+  }
+  return label;
+};
