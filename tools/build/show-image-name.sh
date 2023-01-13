ab87e425d421d127ab31f468994cf59424e02f49
ono-max <onoto1998@gmail.com>
2023-01-13 15:57:10 +0900

    fix

--- tools/build/show-image-name.sh
+++ tools/build/show-image-name.sh
@@ -6,6 +6,8 @@ case $APP in
     "autoops") echo "auto-ops" ;;
     "eventcounter") echo "event-counter" ;;
     "eventpersister") echo "event-persister" ;;
+    "eventpersister-dwh") echo "event-persister-dwh" ;;
+    "goalbatch") echo "goal-batch" ;;
     "metricsevent") echo "metrics-event" ;;
     "opsevent") echo "ops-event" ;;
     *) echo $APP
