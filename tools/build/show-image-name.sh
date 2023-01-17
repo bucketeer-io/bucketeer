d72ac9e650b3b25003b6231d450cac61b46f9f46
ono-max <onoto1998@gmail.com>
2023-01-17 15:47:14 +0900

    chore: add manifest files for event persister dwh

--- tools/build/show-image-name.sh
+++ tools/build/show-image-name.sh
@@ -6,6 +6,7 @@ case $APP in
     "autoops") echo "auto-ops" ;;
     "eventcounter") echo "event-counter" ;;
     "eventpersister") echo "event-persister" ;;
+    "eventpersisterdwh") echo "event-persister-dwh" ;;
     "metricsevent") echo "metrics-event" ;;
     "opsevent") echo "ops-event" ;;
     *) echo $APP
