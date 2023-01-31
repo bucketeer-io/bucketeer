6e2e9dd1edd5ffcedf16279a1c23fe71feadf36f
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2023-01-31 18:52:07 +0900

    chore: change auto ops count key format (#200)

--- pkg/eventpersisterops/persister/evaluation_event.go
+++ pkg/eventpersisterops/persister/evaluation_event.go
@@ -253,11 +253,11 @@ func (u *evalEvtUpdater) newUserCountKey(
 	ruleID, clauseID, featureID, variationID string,
 	featureVersion int32,
 ) string {
-	key := fmt.Sprintf("%s:%s:%s:%d:%s",
-		ruleID,
-		clauseID,
+	key := fmt.Sprintf("%s:%d:%s:%s:%s",
 		featureID,
 		featureVersion,
+		ruleID,
+		clauseID,
 		variationID,
 	)
 	return cache.MakeKey(
