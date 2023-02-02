fd7ab439bc4b7399ce4eab52e85a3064dd106528
Naoto Ono <onoto1998@gmail.com>
2023-02-02 11:52:46 +0900

    Update pkg/autoops/api/api_test.go
    Co-authored-by: Kazuhiro Tashiro <b20288201@gmail.com>

--- pkg/autoops/api/api_test.go
+++ pkg/autoops/api/api_test.go
@@ -223,7 +223,7 @@ func TestCreateAutoOpsRuleMySQL(t *testing.T) {
 					},
 				},
 			},
-			expectedErr: createError(statusOpsEventRateClauseInvalidThredshold, localizer.MustLocalizeWithTemplate(locale.InvalidArgumentError, "thredshold")),
+			expectedErr: createError(statusOpsEventRateClauseInvalidThredshold, localizer.MustLocalizeWithTemplate(locale.InvalidArgumentError, "threshold")),
 		},
 		{
 			desc: "err: ErrOpsEventRateClauseInvalidThredshold: greater",
