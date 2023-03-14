9902c8cd0a642676cf0a3119d8d6ba45d8a56cce
Bimal Gurung <bimalgrg519@gmail.com>
2023-03-14 10:57:25 +0545

    fix(ui): page not found design

--- ui/web-v2/apps/admin/src/components/NotFound/index.tsx
+++ ui/web-v2/apps/admin/src/components/NotFound/index.tsx
@@ -1,9 +1,22 @@
+import { PAGE_PATH_ROOT } from '@/constants/routing';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const NotFound: FC = () => {
   return (
-    <div>
-      <h3>404</h3>
-    </div>
+    <main className="grid min-h-full place-items-center py-24 px-6 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-primary">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <Link to={PAGE_PATH_ROOT} className="btn-submit mt-10">
+          Go back home
+        </Link>
+      </div>
+    </main>
   );
 };
