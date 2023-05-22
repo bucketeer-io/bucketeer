2436a61102701c65b1108cee3c863b693cc9b9c9
Bimal Gurung <bimalgrg519@gmail.com>
2023-05-22 18:48:38 +0545

    add timerange filter and total evaluations

--- ui/web-v2/apps/admin/src/components/TimeseriesStackedLineChart/index.tsx
+++ ui/web-v2/apps/admin/src/components/TimeseriesStackedLineChart/index.tsx
@@ -10,48 +10,22 @@ interface TimeseriesStackedLineChartProps {
   dataLabels: Array<string>;
   timeseries: Array<number>;
   data: Array<Array<number>>;
+  unit: string;
 }
 
 export const TimeseriesStackedLineChart: FC<TimeseriesStackedLineChartProps> =
-  ({ label, dataLabels, timeseries, data }) => {
-    const labels = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-    ];
-
+  ({ label, dataLabels, timeseries, data, unit }) => {
     const chartData = {
-      labels,
-      datasets: [
-        {
-          label: 'Dataset 1',
-          data: [20001, 4, 3, 8, 6, 10, 3],
-          borderColor: 'rgb(255, 99, 132)',
-          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      labels: timeseries.map((t) => new Date(t * 1000)),
+      datasets: dataLabels.map((e, i) => {
+        return {
+          label: e.length > 40 ? `${e.substring(0, 40)}...` : e,
+          data: data[i],
+          backgroundColor: COLORS[i % COLORS.length],
+          borderColor: COLORS[i % COLORS.length],
           fill: false,
-        },
-        {
-          label: 'Dataset 2',
-          data: [1, 4, 13030, 2, 7, 1000000, 5],
-          borderColor: 'rgb(53, 162, 235)',
-          backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          fill: false,
-        },
-      ],
-      // labels: timeseries.map((t) => new Date(t * 1000)),
-      // datasets: dataLabels.map((e, i) => {
-      //   return {
-      //     label: e.length > 40 ? `${e.substring(0, 40)}...` : e,
-      //     data: data[i],
-      //     backgroundColor: COLORS[i % COLORS.length],
-      //     borderColor: COLORS[i % COLORS.length],
-      //     fill: false,
-      //   };
-      // }),
+        };
+      }),
     };
 
     const options: ChartOptions = {
@@ -72,15 +46,15 @@ export const TimeseriesStackedLineChart: FC<TimeseriesStackedLineChartProps> =
         },
       },
       scales: {
-        // xAxes: [
-        //   {
-        //     type: 'time',
-        //     time: {
-        //       unit: 'hour',
-        //       displayFormats: { hour: 'HH:mm' },
-        //     },
-        //   },
-        // ],
+        xAxes: [
+          {
+            type: 'time',
+            time: {
+              unit,
+              // displayFormats: { hour: 'HH:mm' },
+            },
+          },
+        ],
         yAxes: [
           {
             display: true,
@@ -89,6 +63,7 @@ export const TimeseriesStackedLineChart: FC<TimeseriesStackedLineChartProps> =
               // beginAtZero: true,
               userCallback: function (value) {
                 const formatter = Intl.NumberFormat('en', {
+                  //@ts-ignore
                   notation: 'compact',
                 });
                 return formatter.format(value);
--- ui/web-v2/apps/admin/src/pages/feature/evaluation.tsx
+++ ui/web-v2/apps/admin/src/pages/feature/evaluation.tsx
@@ -2,7 +2,10 @@ import React, { FC, memo, useCallback, useEffect, useState } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 import { DetailSkeleton } from '../../components/DetailSkeleton';
-import { FeatureEvaluation } from '../../components/FeatureEvaluation';
+import {
+  FeatureEvaluation,
+  TimeRange,
+} from '../../components/FeatureEvaluation';
 import { AppState } from '../../modules';
 import { getEvaluationTimeseriesCount } from '../../modules/evaluationTimeseriesCount';
 import { useCurrentEnvironment } from '../../modules/me';
@@ -16,28 +19,17 @@ export const FeatureEvaluationPage: FC<FeatureEvaluationPageProps> = memo(
   ({ featureId }) => {
     const dispatch = useDispatch<AppDispatch>();
     const currentEnvironment = useCurrentEnvironment();
-    const isLoading = useSelector<AppState, boolean>(
-      (state) => state.evaluationTimeseriesCount.loading,
-      shallowEqual
-    );
 
     useEffect(() => {
       dispatch(
         getEvaluationTimeseriesCount({
           featureId: featureId,
           environmentNamespace: currentEnvironment.namespace,
-          timeRange: 1,
+          timeRange: TimeRange.THIRTY_DAYS,
         })
       );
     }, [dispatch, featureId, currentEnvironment]);
 
-    if (isLoading) {
-      return (
-        <div className="p-9 bg-gray-100">
-          <DetailSkeleton />
-        </div>
-      );
-    }
     return <FeatureEvaluation featureId={featureId} />;
   }
 );
