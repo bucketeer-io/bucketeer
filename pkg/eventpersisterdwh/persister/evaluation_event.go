66b840de7bc2d3421421aa7c6c20679212b5972e
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2023-07-11 12:10:28 +0900

    chore: change list experiments condition in the event persister (#460)

--- pkg/eventpersisterdwh/persister/evaluation_event.go
+++ pkg/eventpersisterdwh/persister/evaluation_event.go
@@ -38,6 +38,7 @@ type evalEvtWriter struct {
 	writer           storage.EvalEventWriter
 	experimentClient ec.Client
 	flightgroup      singleflight.Group
+	location         *time.Location
 	logger           *zap.Logger
 }
 
@@ -48,6 +49,7 @@ func NewEvalEventWriter(
 	exClient ec.Client,
 	project, ds string,
 	size int,
+	location *time.Location,
 ) (Writer, error) {
 	evt := epproto.EvaluationEvent{}
 	evalQuery, err := writer.NewWriter(
@@ -65,6 +67,7 @@ func NewEvalEventWriter(
 	return &evalEvtWriter{
 		writer:           storage.NewEvalEventWriter(evalQuery, size),
 		experimentClient: exClient,
+		location:         location,
 		logger:           l,
 	}, nil
 }
@@ -202,6 +205,7 @@ func (w *evalEvtWriter) listExperiments(
 					EnvironmentNamespace: environmentNamespace,
 					Statuses: []exproto.Experiment_Status{
 						exproto.Experiment_RUNNING,
+						exproto.Experiment_STOPPED,
 					},
 				})
 				if err != nil {
@@ -220,5 +224,16 @@ func (w *evalEvtWriter) listExperiments(
 		handledCounter.WithLabelValues(codeFailedToListExperiments).Inc()
 		return nil, err
 	}
-	return exp.([]*exproto.Experiment), nil
+	// Filter the stopped experiments
+	// Because the evaluation and goal events may be sent with a delay for many reasons from the client side,
+	// we still calculate the results for two days after it stopped.
+	now := time.Now().In(w.location)
+	experiments := make([]*exproto.Experiment, 0, len(exp.([]*exproto.Experiment)))
+	for _, e := range exp.([]*exproto.Experiment) {
+		if e.Status == exproto.Experiment_STOPPED && now.Unix()-e.StopAt > 2*day {
+			continue
+		}
+		experiments = append(experiments, e)
+	}
+	return experiments, nil
 }
