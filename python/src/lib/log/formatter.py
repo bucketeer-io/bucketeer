10b3f6b8025e13a2b41901cfd53b5608ad9f9728
John Fucking Wick <1354250064mdzz@gmail.com>
2023-07-14 10:19:24 +0800

    chore: remove python calculator code

--- python/src/lib/log/formatter.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-class Formatter(logging.Formatter):
-    def format(self, record):
-        logmsg = super(Formatter, self).format(record)
-        return {"msg": logmsg, "args": record.args}
