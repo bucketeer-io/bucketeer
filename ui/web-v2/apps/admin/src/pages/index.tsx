17ba28ed3604032ff1601aa4d34f1575c4474804
masaaania <b20288201@gmail.com>
2023-08-03 11:02:19 +0900

    remove unused condition

--- ui/web-v2/apps/admin/src/pages/index.tsx
+++ ui/web-v2/apps/admin/src/pages/index.tsx
@@ -60,7 +60,6 @@ export const App: FC = memo(() => {
   useEffect(() => {
     if (
       !window.location.href.includes('localhost') &&
-      GOOGLE_ANALYTICS_ID !== undefined &&
       GOOGLE_ANALYTICS_ID.trim().length > 0
     ) {
       ReactGA.initialize(GOOGLE_ANALYTICS_ID);
