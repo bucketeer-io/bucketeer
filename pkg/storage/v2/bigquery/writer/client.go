2044dfdfc0af17c9596b90de64088f3afff56728
John Fucking Wick <34117229+Ubisoft-potato@users.noreply.github.com>
2023-10-05 04:14:06 +0000

    feat: adapt bigquery emulator

--- pkg/storage/v2/bigquery/writer/client.go
+++ pkg/storage/v2/bigquery/writer/client.go
@@ -21,6 +21,7 @@ import (
 
 	"cloud.google.com/go/bigquery/storage/apiv1/storagepb"
 	"cloud.google.com/go/bigquery/storage/managedwriter"
+	"cloud.google.com/go/bigquery/storage/managedwriter/adapt"
 	"go.uber.org/zap"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
@@ -60,8 +61,14 @@ type Writer interface {
 }
 
 type writer struct {
-	client *managedwriter.ManagedStream
-	opts   *options
+	defaultStream *managedwriter.ManagedStream
+	client        *managedwriter.Client
+	project       string
+	dataset       string
+	table         string
+	desc          protoreflect.MessageDescriptor
+	emulator      bool
+	opts          *options
 	logger *zap.Logger
 }
 
@@ -97,25 +104,7 @@ func NewWriter(
 		return nil, err
 	}
 	var managedStream *managedwriter.ManagedStream
-	if os.Getenv(bigqueryEmulatorHostEnv) != "" {
-		pendingStream, err := c.CreateWriteStream(ctx, &storagepb.CreateWriteStreamRequest{
-			Parent: fmt.Sprintf("projects/%s/datasets/%s/tables/%s", project, dataset, table),
-			WriteStream: &storagepb.WriteStream{
-				Type: storagepb.WriteStream_PENDING,
-			},
-		})
-		if err != nil {
-			return nil, err
-		}
-		managedStream, err = c.NewManagedStream(
-			ctx,
-			managedwriter.WithStreamName(pendingStream.GetName()),
-			managedwriter.WithSchemaDescriptor(protodesc.ToDescriptorProto(desc)),
-		)
-		if err != nil {
-			return nil, err
-		}
-	} else {
+	if os.Getenv(bigqueryEmulatorHostEnv) == "" {
 		managedStream, err = c.NewManagedStream(
 			ctx,
 			managedwriter.WithSchemaDescriptor(protodesc.ToDescriptorProto(desc)),
@@ -130,8 +119,14 @@ func NewWriter(
 		}
 	}
 	return &writer{
-		client: managedStream,
-		opts:   dopts,
+		defaultStream: managedStream,
+		client:        c,
+		project:       project,
+		dataset:       dataset,
+		table:         table,
+		desc:          desc,
+		emulator:      os.Getenv(bigqueryEmulatorHostEnv) != "",
+		opts:          dopts,
 		logger: logger,
 	}, nil
 }
@@ -143,9 +138,12 @@ func (w *writer) AppendRows(
 	fails := make([]int, 0, len(batches))
 	var err error
 	defer record()(operationQuery, &err)
+	if w.emulator {
+		return w.emulatorAppendRows(ctx, batches)
+	}
 	results := []*managedwriter.AppendResult{}
 	for idx, b := range batches {
-		r, err := w.client.AppendRows(ctx, b)
+		r, err := w.defaultStream.AppendRows(ctx, b)
 		if err != nil {
 			w.logger.Error("failed to append rows",
 				zap.Error(err),
@@ -170,7 +168,93 @@ func (w *writer) AppendRows(
 }
 
 func (w *writer) Close() error {
-	return w.client.Close()
+	if w.defaultStream == nil {
+		return nil
+	}
+	return w.defaultStream.Close()
+}
+
+func (w *writer) emulatorAppendRows(
+	ctx context.Context,
+	batches [][][]byte,
+) ([]int, error) {
+	pendingStream, err := w.client.CreateWriteStream(ctx, &storagepb.CreateWriteStreamRequest{
+		Parent: fmt.Sprintf("projects/%s/datasets/%s/tables/%s", w.project, w.dataset, w.table),
+		WriteStream: &storagepb.WriteStream{
+			Type: storagepb.WriteStream_PENDING,
+		},
+	})
+	if err != nil {
+		w.opts.logger.Error("failed to create pending stream for emulator",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	descriptor, err := adapt.NormalizeDescriptor(w.desc)
+	if err != nil {
+		w.opts.logger.Error("failed to normalize descriptor for emulator",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	managedStream, err := w.client.NewManagedStream(
+		ctx,
+		managedwriter.WithStreamName(pendingStream.GetName()),
+		managedwriter.WithSchemaDescriptor(descriptor),
+	)
+	if err != nil {
+		w.opts.logger.Error("failed to create managed stream for emulator",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	fails := make([]int, 0, len(batches))
+	results := []*managedwriter.AppendResult{}
+	for idx, b := range batches {
+		r, err := managedStream.AppendRows(ctx, b, managedwriter.WithOffset(0))
+		if err != nil {
+			w.opts.logger.Error("failed to append rows for emulator",
+				zap.Error(err),
+			)
+			fails = append(fails, idx)
+		}
+		results = append(results, r)
+	}
+	for idx, r := range results {
+		_, err := r.GetResult(ctx)
+		if err != nil {
+			fails = append(fails, idx)
+		}
+	}
+	rowCount, err := managedStream.Finalize(ctx)
+	if err != nil {
+		w.opts.logger.Error("failed to finalize for emulator",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	w.opts.logger.Info("row count",
+		zap.Int64("count", rowCount),
+	)
+	req := &storagepb.BatchCommitWriteStreamsRequest{
+		Parent:       managedwriter.TableParentFromStreamName(managedStream.StreamName()),
+		WriteStreams: []string{managedStream.StreamName()},
+	}
+
+	resp, err := w.client.BatchCommitWriteStreams(ctx, req)
+	if err != nil {
+		w.opts.logger.Error("failed to batch commit write streams for emulator",
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	if len(resp.GetStreamErrors()) > 0 {
+		w.opts.logger.Error("failed to batch commit write streams for emulator",
+			zap.Any("errors", resp.GetStreamErrors()),
+		)
+		return nil, err
+	}
+	return getUniqueFails(fails), err
 }
 
 func getUniqueFails(fs []int) []int {
