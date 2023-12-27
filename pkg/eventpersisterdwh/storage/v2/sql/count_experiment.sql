8da07d607a7bb1aff7f11b99101047ed7b5a7e82
John Fucking Wick <1354250064mdzz@gmail.com>
2023-12-27 10:50:07 +0800

    chore: format sql

--- pkg/eventpersisterdwh/storage/v2/sql/count_experiment.sql
+++ pkg/eventpersisterdwh/storage/v2/sql/count_experiment.sql
@@ -1,4 +1,19 @@
-SELECT (SELECT count(*) FROM experiment WHERE status = 1) + (SELECT count(*)
-                                                             FROM experiment
-                                                             WHERE status = 2
-                                                               AND stop_at > UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 2 DAY))) as count;
+SELECT
+    (
+        SELECT
+            count(*)
+        FROM
+            experiment
+        WHERE
+            status = 1
+    ) + (
+        SELECT
+            count(*)
+        FROM
+            experiment
+        WHERE
+            status = 2
+          AND stop_at > UNIX_TIMESTAMP(
+                DATE_SUB(NOW(), INTERVAL 2 DAY)
+          )
+    ) as count;
