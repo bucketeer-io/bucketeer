2f54b68b9a3737ee64b53cc3f6e29cd15b0b6464
John Fucking Wick <34117229+Ubisoft-potato@users.noreply.github.com>
2024-01-23 11:04:10 +0800

    chore: build dev container base image (#711)

---
+++ .github/.devcontainer/local-features/setup-user/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+
+USERNAME=${USERNAME:-"codespace"}
+
+set -eux
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+  exit 1
+fi
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" >/etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo_if() {
+  COMMAND="$*"
+  if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
+    su - "$USERNAME" -c "$COMMAND"
+  else
+    "$COMMAND"
+  fi
+}
+
+HOME_DIR="/home/${USERNAME}/"
+chown -R ${USERNAME}:${USERNAME} ${HOME_DIR}
+chmod -R g+r+w "${HOME_DIR}"
+find "${HOME_DIR}" -type d | xargs -n 1 chmod g+s
+
+echo "Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/share:/home/${USERNAME}/.local/bin:${PATH}\"" >>/etc/sudoers.d/$USERNAME
+
+echo "Done!"
