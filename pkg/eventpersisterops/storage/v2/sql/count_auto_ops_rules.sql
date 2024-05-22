95660bf4bf5050f08ef358444f60048694c2a382
kakcy <yokeyphone@gmail.com>
2024-05-22 10:57:17 +0900

    feat: add eventpersisterops of multi schedule

--- pkg/eventpersisterops/storage/v2/sql/count_auto_ops_rules.sql
+++ pkg/eventpersisterops/storage/v2/sql/count_auto_ops_rules.sql
@@ -1,6 +1,5 @@
 SELECT COUNT(*)
 FROM auto_ops_rule
-WHERE triggered_at = 0
-  AND deleted = 0
-  AND JSON_EXTRACT(JSON_EXTRACT(clauses, '$[*].clause'), '$[0].type_url') != 'type.googleapis.com/bucketeer.autoops.DatetimeClause'
-  -- Schedule operation is not based on events, so we don't include in the result
+WHERE status = 0   -- waiting
+  AND status = 4   -- deleted
+  AND ops_type != 2 -- Schedule operation is not based on events, so we don't include in the result
