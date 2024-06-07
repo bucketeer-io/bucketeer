e7cc0827d42ae6818ec9587ab3a2452b244b4340
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2024-06-07 10:12:23 +0900

    fix: including archived features in the count query (#996)
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/batch/storage/v2/sql/count_auto_ops_rules.sql
+++ pkg/batch/storage/v2/sql/count_auto_ops_rules.sql
@@ -1,6 +1,10 @@
 SELECT COUNT(*)
 FROM auto_ops_rule
-WHERE triggered_at = 0
-  AND deleted = 0
-  AND JSON_EXTRACT(JSON_EXTRACT(clauses, '$[*].clause'), '$[0].type_url') != 'type.googleapis.com/bucketeer.autoops.DatetimeClause'
+LEFT JOIN feature
+ON auto_ops_rule.feature_id = feature.id
+AND auto_ops_rule.environment_namespace = feature.environment_namespace
+WHERE feature.archived = 0
+  AND auto_ops_rule.triggered_at = 0
+  AND auto_ops_rule.deleted = 0
+  AND JSON_EXTRACT(JSON_EXTRACT(auto_ops_rule.clauses, '$[*].clause'), '$[0].type_url') != 'type.googleapis.com/bucketeer.autoops.DatetimeClause'
   -- Schedule operation is not based on events, so we don't include in the result
