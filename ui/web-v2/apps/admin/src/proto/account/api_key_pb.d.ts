62356abdd4b828cd4cb0294a6007d7723c8789a6
Kenta Kozuka <kenta.kozuka@gmail.com>
2024-06-10 12:01:08 +0900

    chore: add api key roles for public api (#998)
    * chore: add api key roles for public api
    
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>
    
    * update descriptor
    
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>
    
    * rename roles
    
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>
    
    * fix: format error
    
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>
    
    ---------
    
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>

--- ui/web-v2/apps/admin/src/proto/account/api_key_pb.d.ts
+++ ui/web-v2/apps/admin/src/proto/account/api_key_pb.d.ts
@@ -47,6 +47,9 @@ export namespace APIKey {
     UNKNOWN: 0;
     SDK_CLIENT: 1;
     SDK_SERVER: 2;
+    PUBLIC_API_READ_ONLY: 3;
+    PUBLIC_API_WRITE: 4;
+    PUBLIC_API_ADMIN: 5;
   }
 
   export const Role: RoleMap;
--- ui/web-v2/apps/admin/src/proto/account/api_key_pb.js
+++ ui/web-v2/apps/admin/src/proto/account/api_key_pb.js
@@ -246,7 +246,10 @@ proto.bucketeer.account.APIKey.serializeBinaryToWriter = function(message, write
 proto.bucketeer.account.APIKey.Role = {
   UNKNOWN: 0,
   SDK_CLIENT: 1,
-  SDK_SERVER: 2
+  SDK_SERVER: 2,
+  PUBLIC_API_READ_ONLY: 3,
+  PUBLIC_API_WRITE: 4,
+  PUBLIC_API_ADMIN: 5
 };
 
 /**
