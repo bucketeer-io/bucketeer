6290a96a4f1e00c31964ae05008c622ba0b1f1a7
Cyka <1354250064mdzz@gmail.com>
2024-06-27 17:32:53 +0800

    Revert "chore: remove unuse field"
    This reverts commit df4dffca57ff6df800c2f1b075086e4d284c51c6.

--- ui/web-v2/src/proto/auth/token_pb.d.ts
+++ ui/web-v2/src/proto/auth/token_pb.d.ts
@@ -16,6 +16,9 @@ export class Token extends jspb.Message {
   getExpiry(): number;
   setExpiry(value: number): void;
 
+  getIdToken(): string;
+  setIdToken(value: string): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): Token.AsObject;
   static toObject(includeInstance: boolean, msg: Token): Token.AsObject;
@@ -40,5 +43,41 @@ export namespace Token {
     tokenType: string;
     refreshToken: string;
     expiry: number;
+    idToken: string;
+  };
+}
+
+export class IDTokenSubject extends jspb.Message {
+  getUserId(): string;
+  setUserId(value: string): void;
+
+  getConnId(): string;
+  setConnId(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): IDTokenSubject.AsObject;
+  static toObject(
+    includeInstance: boolean,
+    msg: IDTokenSubject
+  ): IDTokenSubject.AsObject;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: IDTokenSubject,
+    writer: jspb.BinaryWriter
+  ): void;
+  static deserializeBinary(bytes: Uint8Array): IDTokenSubject;
+  static deserializeBinaryFromReader(
+    message: IDTokenSubject,
+    reader: jspb.BinaryReader
+  ): IDTokenSubject;
+}
+
+export namespace IDTokenSubject {
+  export type AsObject = {
+    userId: string;
+    connId: string;
   };
 }
