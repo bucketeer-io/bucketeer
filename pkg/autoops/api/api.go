608ebc8db89f521ff47bb23e56db829ce938a39e
kakcy <43775118+kakcy@users.noreply.github.com>
2024-07-04 18:53:45 +0900

    Update pkg/autoops/api/api.go
    Co-authored-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/autoops/api/api.go
+++ pkg/autoops/api/api.go
@@ -1056,6 +1056,7 @@ func (s *AutoOpsService) ExecuteAutoOps(
 		for _, c := range autoOpsRule.Clauses {
 			if c.Id == req.ExecuteAutoOpsRuleCommand.ClauseId {
 				executeClause = c
+				break
 			}
 		}
 
