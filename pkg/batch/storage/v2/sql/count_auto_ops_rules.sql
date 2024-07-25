dffb736c82749fe6457323f4ad33fcb537dad960
kakcy <yokeyphone@gmail.com>
2024-07-25 20:20:29 +0900

    fix: compatible with multiple schedule

--- pkg/batch/storage/v2/sql/count_auto_ops_rules.sql
+++ pkg/batch/storage/v2/sql/count_auto_ops_rules.sql
@@ -4,7 +4,7 @@ LEFT JOIN feature
 ON auto_ops_rule.feature_id = feature.id
 AND auto_ops_rule.environment_namespace = feature.environment_namespace
 WHERE feature.archived = 0
-  AND auto_ops_rule.triggered_at = 0
+  AND auto_ops_rule.status IN (0, 1) -- 0: WAITING, 1: RUNNING
   AND auto_ops_rule.deleted = 0
-  AND JSON_EXTRACT(JSON_EXTRACT(auto_ops_rule.clauses, '$[*].clause'), '$[0].type_url') != 'type.googleapis.com/bucketeer.autoops.DatetimeClause'
+  AND auto.ops_rule.ops_type != 2
   -- Schedule operation is not based on events, so we don't include in the result
