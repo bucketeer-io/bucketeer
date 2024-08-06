030c675a20d2910e7f75432d0f3342fd1495bd4c
kakcy <yokeyphone@gmail.com>
2024-08-06 16:03:58 +0900

    chore: migrated auto_ops_rule table to delete triggered_at

---
+++ migration/mysql/20240806065947_update_auto_ops_rule_table.sql
@@ -0,0 +1,2 @@
+-- Create "atlas_schema_revisions" table
+CREATE TABLE `atlas_schema_revisions` (`version` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, `type` bigint unsigned NOT NULL DEFAULT 2, `applied` bigint NOT NULL DEFAULT 0, `total` bigint NOT NULL DEFAULT 0, `executed_at` timestamp NOT NULL, `execution_time` bigint NOT NULL, `error` longtext NULL, `error_stmt` longtext NULL, `hash` varchar(255) NOT NULL, `partial_hashes` json NULL, `operator_version` varchar(255) NOT NULL, PRIMARY KEY (`version`)) CHARSET utf8mb4 COLLATE utf8mb4_bin;
--- pkg/autoops/storage/v2/sql/auto_ops_rule/insert_auto_ops_rule.sql
+++ pkg/autoops/storage/v2/sql/auto_ops_rule/insert_auto_ops_rule.sql
@@ -3,12 +3,11 @@ INSERT INTO auto_ops_rule (
     feature_id,
     ops_type,
     clauses,
-    triggered_at,
     created_at,
     updated_at,
     deleted,
     status,
     environment_namespace
 ) VALUES (
-    ?, ?, ?, ?, 0, ?, ?, ?, ?, ?
+    ?, ?, ?, ?, ?, ?, ?, ?, ?
 )
