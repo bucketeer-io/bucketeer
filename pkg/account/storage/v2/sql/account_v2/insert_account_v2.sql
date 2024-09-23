c9f4b1406e2f5258506e721a716ad1b15b67e71a
Cyka <1354250064mdzz@gmail.com>
2024-09-23 10:29:17 +0800

    feat: new user registration

--- pkg/account/storage/v2/sql/account_v2/insert_account_v2.sql
+++ pkg/account/storage/v2/sql/account_v2/insert_account_v2.sql
@@ -1,11 +1,13 @@
 INSERT INTO account_v2 (
     email,
-    name,
+    first_name,
+    last_name,
+    language,
     avatar_image_url,
     organization_id,
     organization_role,
     environment_roles,
     disabled,
     created_at,
-    updated_at)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    updated_at
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
