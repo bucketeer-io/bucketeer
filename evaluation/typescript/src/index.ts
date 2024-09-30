d596fc0be3e237aa7b5a2c49c2b13bf8bd2b47e9
duyhungtnn <duyhung71089@gmail.com>
2024-09-30 08:22:18 +0700

    Update index.ts

---
+++ evaluation/typescript/src/index.ts
@@ -0,0 +1,35 @@
+import { Evaluator } from './evaluation';
+import { NewUserEvaluations } from './userEvaluation';
+import {
+  createClause,
+  createFixedStrategy,
+  createRolloutStrategy,
+  createReason,
+  createRule,
+  createPrerequisite,
+  creatFeature,
+  createEvaluation,
+  createSegmentUser,
+  createStrategy,
+  createTarget,
+  createUser,
+  createVariation
+} from './modelFactory';
+
+export { Evaluator };
+export { NewUserEvaluations };
+export {
+  createClause,
+  createFixedStrategy,
+  createRolloutStrategy,
+  createReason,
+  createRule,
+  createPrerequisite,
+  creatFeature,
+  createEvaluation,
+  createSegmentUser,
+  createStrategy,
+  createTarget,
+  createUser,
+  createVariation
+};
