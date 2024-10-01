2a058173479801ea5c1a7dcd613909c2c1d8188e
John Fucking Wick <34117229+Ubisoft-potato@users.noreply.github.com>
2024-10-01 07:23:57 +0000

    chore: proto mockgen

--- ui/web-v2/src/proto/auth/service_pb.d.ts
+++ ui/web-v2/src/proto/auth/service_pb.d.ts
@@ -121,9 +121,6 @@ export class ExchangeTokenResponse extends jspb.Message {
   getToken(): proto_auth_token_pb.Token | undefined;
   setToken(value?: proto_auth_token_pb.Token): void;
 
-  getNewuser(): boolean;
-  setNewuser(value: boolean): void;
-
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): ExchangeTokenResponse.AsObject;
   static toObject(
@@ -148,7 +145,6 @@ export class ExchangeTokenResponse extends jspb.Message {
 export namespace ExchangeTokenResponse {
   export type AsObject = {
     token?: proto_auth_token_pb.Token.AsObject;
-    newuser: boolean;
   };
 }
 
