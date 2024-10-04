63619af5345fd291353efeb8b576d05461b2c062
Cyka <1354250064mdzz@gmail.com>
2024-10-04 16:39:43 +0800

    feat: add redis copy hack script

---
+++ hack/redis-data-copy/command.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"go.uber.org/zap"
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/bucketeer-io/bucketeer/pkg/cli"
+	"github.com/bucketeer-io/bucketeer/pkg/metrics"
+	v3 "github.com/bucketeer-io/bucketeer/pkg/redis/v3"
+)
+
+type command struct {
+	*kingpin.CmdClause
+	sourceRedisAddress      *string
+	destinationRedisAddress *string
+}
+
+func registerCommand(r cli.CommandRegistry, p cli.ParentCommand) *command {
+	cmd := p.Command("copy", "Copy data from source Redis to destination Redis")
+	command := &command{
+		CmdClause:               cmd,
+		sourceRedisAddress:      cmd.Flag("source", "Source Redis address").Required().String(),
+		destinationRedisAddress: cmd.Flag("destination", "Destination Redis address").Required().String(),
+	}
+	r.RegisterCommand(command)
+	return command
+}
+
+func (c *command) Run(ctx context.Context, metrics metrics.Metrics, logger *zap.Logger) error {
+	srcClient, err := v3.NewClient(*c.sourceRedisAddress,
+		v3.WithLogger(logger),
+	)
+	if err != nil {
+		logger.Error("Error creating source Redis client", zap.Error(err))
+		return err
+	}
+	defer srcClient.Close()
+
+	dstClient, err := v3.NewClient(*c.destinationRedisAddress,
+		v3.WithLogger(logger),
+	)
+	if err != nil {
+		logger.Error("Error creating destination Redis client", zap.Error(err))
+		return err
+	}
+	defer dstClient.Close()
+
+	if err := c.scanAndCopyBatch(srcClient, dstClient, logger); err != nil {
+		logger.Error("Error during scan and copy process", zap.Error(err))
+		return err
+	}
+
+	logger.Info("Data copy completed")
+	return nil
+}
+
+func (c *command) scanAndCopyBatch(src, dst v3.Client, logger *zap.Logger) error {
+	var cursor uint64
+	batchSize := int64(100)
+
+	for {
+		nextCursor, keys, err := src.Scan(cursor, "*", batchSize)
+		if err != nil {
+			return fmt.Errorf("error scanning keys from source Redis: %v", err)
+		}
+
+		if err := c.copyBatch(src, dst, keys); err != nil {
+			logger.Error("Error copying batch", zap.Error(err))
+		}
+
+		if nextCursor == 0 {
+			break
+		}
+		cursor = nextCursor
+	}
+
+	return nil
+}
+
+func (c *command) copyBatch(src, dst v3.Client, keys []string) error {
+	for _, key := range keys {
+		value, err := src.Get(key)
+		if err != nil {
+			if errors.Is(err, v3.ErrNil) {
+				log.Printf("Key not found: %s", key)
+				continue
+			}
+			return fmt.Errorf("error getting value for key %s: %v", key, err)
+		}
+
+		err = dst.Set(key, value, 0)
+		if err != nil {
+			return fmt.Errorf("error setting value for key %s: %v", key, err)
+		}
+	}
+
+	log.Printf("Successfully copied batch of %d keys", len(keys))
+	return nil
+}
