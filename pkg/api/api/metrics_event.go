597ce7a316c122db8365693561c069d2cf53558d
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2024-10-06 14:21:21 +0900

    chore: add log for error metrics from sdk client
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/api/api/metrics_event.go
+++ pkg/api/api/metrics_event.go
@@ -395,6 +395,16 @@ func (s *grpcGatewayService) saveErrorCount(
 	if labels != nil {
 		tag = labels["tag"]
 	}
+	s.logger.Error("Error metric details",
+		zap.String("projectId", projectID),
+		zap.String("environment", env),
+		zap.String("tag", tag),
+		zap.Any("labels", labels),
+		zap.String("errorType", errorType),
+		zap.String("apiId", apiID.String()),
+		zap.String("sdkVersion", event.SdkVersion),
+		zap.String("sourceId", event.SourceId.String()),
+	)
 	sdkErrorCounter.WithLabelValues(
 		projectID,
 		env,
