730d7df758f54a34ac452aff066f929f8542e341
sevenninhlee <lephuocninh7118@gmail.com>
2024-10-19 23:10:52 +0700

    implement tabs link

--- ui/dashboard/src/pages/organization-details/page-content.tsx
+++ ui/dashboard/src/pages/organization-details/page-content.tsx
@@ -1,46 +1,66 @@
-import { useState } from 'react';
+import { Navigate, Route, Routes, useParams } from 'react-router-dom';
+import {
+  PAGE_PATH_ORGANIZATIONS,
+  PAGE_PATH_PROJECTS,
+  PAGE_PATH_SETTINGS,
+  PAGE_PATH_USERS
+} from 'constants/routing';
 import { useTranslation } from 'i18n';
 import { Organization } from '@types';
-import { Tabs, TabsList, TabsTrigger, TabsContent } from 'components/tabs';
+import { Tabs, TabsList, TabsContent, TabsLink } from 'components/tabs-link';
 import PageLayout from 'elements/page-layout';
 import OrganizationProjects from './projects';
 import OrganizationSettings from './settings';
+import { TabItem } from './types';
 import OrganizationUsers from './users';
 
 const PageContent = ({ organization }: { organization: Organization }) => {
   const { t } = useTranslation(['common']);
-  const [selectedTab, setSelectedTab] = useState('PROJECTS');
+  const { organizationId } = useParams();
+  const url = `${PAGE_PATH_ORGANIZATIONS}/${organizationId}`;
 
-  const getTabContent = () => {
-    switch (selectedTab) {
-      case 'PROJECTS':
-        return <OrganizationProjects />;
-
-      case 'USERS':
-        return <OrganizationUsers />;
-
-      case 'SETTINGS':
-        return <OrganizationSettings organization={organization} />;
-
-      default:
-        return null;
+  const organizationTabs: Array<TabItem> = [
+    {
+      title: t(`projects`),
+      to: PAGE_PATH_PROJECTS
+    },
+    {
+      title: t(`users`),
+      to: PAGE_PATH_USERS
+    },
+    {
+      title: t(`settings`),
+      to: PAGE_PATH_SETTINGS
     }
-  };
+  ];
 
   return (
     <PageLayout.Content className="pt-4">
-      <Tabs
-        defaultValue={selectedTab}
-        onValueChange={setSelectedTab}
-        className="flex-1 flex h-full flex-col"
-      >
+      <Tabs className="flex-1 flex h-full flex-col">
         <TabsList>
-          <TabsTrigger value="PROJECTS">{t(`projects`)}</TabsTrigger>
-          <TabsTrigger value="USERS">{t(`users`)}</TabsTrigger>
-          <TabsTrigger value="SETTINGS">{t(`settings`)}</TabsTrigger>
+          {organizationTabs.map((item, index) => (
+            <TabsLink key={index} to={`${url}${item.to}`}>
+              {item.title}
+            </TabsLink>
+          ))}
         </TabsList>
-        <TabsContent value={selectedTab} className="pt-2">
-          {getTabContent()}
+
+        <TabsContent className="pt-2">
+          <Routes>
+            <Route
+              index
+              element={<Navigate to={`${url}${PAGE_PATH_PROJECTS}`} replace />}
+            />
+            <Route
+              path={PAGE_PATH_PROJECTS}
+              element={<OrganizationProjects />}
+            />
+            <Route path={PAGE_PATH_USERS} element={<OrganizationUsers />} />
+            <Route
+              path={PAGE_PATH_SETTINGS}
+              element={<OrganizationSettings organization={organization} />}
+            />
+          </Routes>
         </TabsContent>
       </Tabs>
     </PageLayout.Content>
