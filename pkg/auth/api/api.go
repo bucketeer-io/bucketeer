5f7d4b4c0a1a0f4651f8d933f8d7d4bd04012684
John Fucking Wick <34117229+Ubisoft-potato@users.noreply.github.com>
2024-10-21 03:38:00 +0000

    chore: update last_seen

--- pkg/auth/api/api.go
+++ pkg/auth/api/api.go
@@ -342,14 +342,13 @@ func (s *authService) updateUserInfoForOrganizations(
 			OrganizationId: org.Id,
 		})
 		if err != nil {
-			if status.Code(err) != codes.NotFound {
-				s.logger.Error(
-					"Failed to get account",
-					zap.Error(err),
-					zap.String("organizationId", org.Id),
-				)
-				continue
-			}
+			s.logger.Error(
+				"Failed to get account",
+				zap.Error(err),
+				zap.String("email", userInfo.Email),
+				zap.String("organizationId", org.Id),
+			)
+			continue
 		}
 
 		if account.Account.LastSeen == 0 {
