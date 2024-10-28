af6d07204f62162c3af7a0dd95124b7fdf8938bb
Cyka <1354250064mdzz@gmail.com>
2024-10-28 17:19:30 +0800

    chore: rebase main

--- migration/mysql/20241021012743_update_account_v2_table.sql
+++
@@ -1,2 +0,0 @@
--- Modify "account_v2" table
-ALTER TABLE `account_v2` ADD COLUMN `first_name` varchar(255) NULL, ADD COLUMN `last_name` varchar(255) NULL, ADD COLUMN `language` varchar(10) NULL, ADD COLUMN `last_seen` bigint NOT NULL DEFAULT 0;
