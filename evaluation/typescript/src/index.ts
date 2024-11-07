7fa105159b07cdc9a5053747d5411273dfd97040
duyhungtnn <duyhung71089@gmail.com>
2024-11-07 14:31:04 +0700

    fix: missing export Evaluation, UserEvaluations

--- evaluation/typescript/src/index.ts
+++ evaluation/typescript/src/index.ts
@@ -2,6 +2,8 @@ import { Evaluator } from './evaluation';
 import { SegmentUser, SegmentUsers } from './proto/feature/segment_pb';
 import { Feature } from './proto/feature/feature_pb';
 import { NewUserEvaluations } from './userEvaluation';
+import { User } from './proto/user/user_pb';
+import { Evaluation, UserEvaluations } from './proto/feature/evaluation_pb';
 import {
   createClause,
   createFixedStrategy,
@@ -26,9 +28,8 @@ import {
 import { SourceId } from './proto/event/client/event_pb';
 import { GatewayClient, ServiceError } from './proto/gateway/service_pb_service';
 
-export { Evaluator };
-export { NewUserEvaluations };
-export { SegmentUser, SegmentUsers, Feature };
+export { Evaluator, NewUserEvaluations, Evaluation, UserEvaluations };
+export { User, SegmentUser, SegmentUsers, Feature };
 export {
   createClause,
   createFixedStrategy,
