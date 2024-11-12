50c4c3a62e1756dbe6eb2b5474c8f6b12a92b894
Hien Vu <112093533+hvn2k1@users.noreply.github.com>
2024-11-12 10:13:26 +0700

    test: trigger callback func inside mock Transaction
    - Trigger callback functions inside transaction in create account v2 test

--- pkg/account/api/account_test.go
+++ pkg/account/api/account_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 	"time"
 
+	publishermock "github.com/bucketeer-io/bucketeer/pkg/pubsub/publisher/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
@@ -359,9 +360,32 @@ func TestCreateAccountV2NoCommandMySQL(t *testing.T) {
 					},
 				}, nil)
 
-				s.accountStorage.(*accstoragemock.MockAccountStorage).EXPECT().RunInTransaction(
+				s.accountStorage.(*accstoragemock.MockAccountStorage).EXPECT().GetAccountV2(
+					gomock.Any(), gomock.Any(), gomock.Any(),
+				).Return(&domain.AccountV2{
+					AccountV2: &accountproto.AccountV2{
+						Email:            "bucketeer@example.com",
+						FirstName:        "Test",
+						LastName:         "User",
+						Language:         "en",
+						OrganizationRole: accountproto.AccountV2_Role_Organization_ADMIN,
+					},
+				}, nil)
+
+				s.publisher.(*publishermock.MockPublisher).EXPECT().Publish(
 					gomock.Any(), gomock.Any(),
 				).Return(nil)
+
+				s.accountStorage.(*accstoragemock.MockAccountStorage).EXPECT().UpdateAccountV2(
+					gomock.Any(), gomock.Any(),
+				).Return(nil)
+
+				s.accountStorage.(*accstoragemock.MockAccountStorage).EXPECT().RunInTransaction(
+					gomock.Any(), gomock.Any(),
+				).Do(func(ctx context.Context, fn func() error) {
+					err := fn()
+					require.NoError(t, err)
+				}).Return(nil)
 			},
 			expectedErr: nil,
 		},
