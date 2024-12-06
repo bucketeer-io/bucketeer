fc5e47d2b85c72724c8b71bea1d0e04712fbfde3
Cyka <1354250064mdzz@gmail.com>
2024-12-06 10:37:33 +0800

    chore: update regex

--- pkg/eventcounter/api/api.go
+++ pkg/eventcounter/api/api.go
@@ -537,12 +537,6 @@ func (s *eventCounterService) countUniqueUser(
 		err = append(err, e)
 		return
 	}
-	// Set expiration time for pfmerge key in case delete fails
-	defer func() {
-		if e := s.evaluationCountCacher.DeleteKey(key); e != nil {
-			err = append(err, e)
-		}
-	}()
 	c, e := s.evaluationCountCacher.GetUserCount(key)
 	count = float64(c)
 	if e != nil {
