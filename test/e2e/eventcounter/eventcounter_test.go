c2055e99ee0f2b58e0bd09de095dfb8d11624b90
Cyka <1354250064mdzz@gmail.com>
2024-12-09 13:37:21 +0800

    test: remove sleep code

--- test/e2e/eventcounter/eventcounter_test.go
+++ test/e2e/eventcounter/eventcounter_test.go
@@ -129,11 +129,6 @@ func TestGrpcExperimentGoalCount(t *testing.T) {
 		variations[v.Value] = v
 	}
 
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
-
 	grpcRegisterGoalEvent(t, goalIDs[0], userID, tag, float64(0.2), time.Now().Unix())
 	grpcRegisterGoalEvent(t, goalIDs[0], userID, tag, float64(0.3), time.Now().Unix())
 	// This event will be ignored because the timestamp is older than the experiment startAt time stamp
@@ -262,11 +257,6 @@ func TestExperimentGoalCount(t *testing.T) {
 		variations[v.Value] = v
 	}
 
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
-
 	registerGoalEvent(t, goalIDs[0], userID, tag, float64(0.2), time.Now().Unix())
 	registerGoalEvent(t, goalIDs[0], userID, tag, float64(0.3), time.Now().Unix())
 	// This event will be ignored because the timestamp is older than the experiment startAt time stamp
@@ -393,11 +383,6 @@ func TestGrpcExperimentResult(t *testing.T) {
 	stopAt := startAt.Local().Add(time.Hour * 2)
 	experiment := createExperimentWithMultiGoals(
 		ctx, t, experimentClient, "TestGrpcExperimentResult", featureID, goalIDs, f.Variations[0].Id, startAt, stopAt)
-
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
 
 	// CVRs is 3/4
 	// Register goal variation
@@ -550,11 +535,6 @@ func TestExperimentResult(t *testing.T) {
 	stopAt := startAt.Local().Add(time.Hour * 2)
 	experiment := createExperimentWithMultiGoals(
 		ctx, t, experimentClient, "TestExperimentResult", featureID, goalIDs, f.Variations[0].Id, startAt, stopAt)
-
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
 
 	// CVRs is 3/4
 	// Register goal variation
@@ -713,11 +693,6 @@ func TestGrpcMultiGoalsEventCounter(t *testing.T) {
 		variationIDs = append(variationIDs, v.Id)
 		variations[v.Value] = v
 	}
-
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
 
 	grpcRegisterGoalEvent(t, goalIDs[0], userIDs[0], tag, float64(0.3), time.Now().Unix())
 	grpcRegisterGoalEvent(t, goalIDs[0], userIDs[0], tag, float64(0.3), time.Now().Unix())
@@ -936,11 +911,6 @@ func TestMultiGoalsEventCounter(t *testing.T) {
 		variations[v.Value] = v
 	}
 
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
-
 	registerGoalEvent(t, goalIDs[0], userIDs[0], tag, float64(0.3), time.Now().Unix())
 	registerGoalEvent(t, goalIDs[0], userIDs[0], tag, float64(0.3), time.Now().Unix())
 	registerGoalEvent(t, goalIDs[1], userIDs[1], tag, float64(0.2), time.Now().Unix())
@@ -1155,11 +1125,6 @@ func TestHTTPTrack(t *testing.T) {
 		variations[v.Value] = v
 	}
 
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
-
 	// Send track events.
 	sendHTTPTrack(t, userID, goalIDs[0], tag, value)
 	registerEvaluationEvent(t, featureID, f.Version, userID, f.Variations[0].Id, tag, reason)
@@ -1283,11 +1248,6 @@ func TestGrpcExperimentEvaluationEventCount(t *testing.T) {
 		startAt, stopAt,
 	)
 
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
-
 	grpcRegisterEvaluationEvent(t, featureID, f.Version, userID, variations[variationVarA].Id, tag, reason)
 
 	for i := 0; i < retryTimes; i++ {
@@ -1410,11 +1370,6 @@ func TestExperimentEvaluationEventCount(t *testing.T) {
 		startAt, stopAt,
 	)
 
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
-
 	registerEvaluationEvent(t, featureID, f.Version, userID, variations[variationVarA].Id, tag, reason)
 	for i := 0; i < retryTimes; i++ {
 		if i == retryTimes-1 {
@@ -1494,11 +1449,6 @@ func TestGetEvaluationTimeseriesCount(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to get feature. ID: %s. Error: %v", featureID, err)
 	}
-
-	// Wait for the event-persister-dwh subscribe to the pubsub
-	// The batch runs every minute, so we give a extra 10 seconds
-	// to ensure that it will subscribe correctly.
-	time.Sleep(70 * time.Second)
 
 	// Register variation
 	registerEvaluationEvent(t, featureID, f.Version, userIDs[0], f.Variations[0].Id, tag, nil)
