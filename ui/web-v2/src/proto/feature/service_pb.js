d2f249fc19763c4b5e90ac1da624242eb93e80f5
Hien Vu <112093533+hvn2k1@users.noreply.github.com>
2025-01-06 11:43:21 +0700

    implement REST list tags

--- ui/web-v2/src/proto/feature/service_pb.js
+++ ui/web-v2/src/proto/feature/service_pb.js
@@ -27,6 +27,8 @@ var google_api_annotations_pb = require('../../google/api/annotations_pb.js');
 goog.object.extend(proto, google_api_annotations_pb);
 var google_protobuf_wrappers_pb = require('google-protobuf/google/protobuf/wrappers_pb.js');
 goog.object.extend(proto, google_protobuf_wrappers_pb);
+var protoc$gen$openapiv2_options_annotations_pb = require('../../protoc-gen-openapiv2/options/annotations_pb.js');
+goog.object.extend(proto, protoc$gen$openapiv2_options_annotations_pb);
 var proto_feature_command_pb = require('../../proto/feature/command_pb.js');
 goog.object.extend(proto, proto_feature_command_pb);
 var proto_feature_feature_pb = require('../../proto/feature/feature_pb.js');
