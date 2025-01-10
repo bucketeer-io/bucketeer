7b45d816e7b324a82b9400a5e6f5180296ffcd55
Cyka <1354250064mdzz@gmail.com>
2025-01-10 14:01:46 +0800

    chore: remove environment_namespace column

---
+++ migration/mysql/20250110054703_remove_environment_namespace_column.sql
@@ -0,0 +1,19 @@
+-- Remove environment_namespace column from all tables
+ALTER TABLE feature DROP COLUMN environment_namespace;
+ALTER TABLE account DROP COLUMN environment_namespace;
+ALTER TABLE api_key DROP COLUMN environment_namespace;
+ALTER TABLE audit_log DROP COLUMN environment_namespace;
+ALTER TABLE auto_ops_rule DROP COLUMN environment_namespace;
+ALTER TABLE experiment DROP COLUMN environment_namespace;
+ALTER TABLE experiment_result DROP COLUMN environment_namespace;
+ALTER TABLE feature_last_used_info DROP COLUMN environment_namespace;
+ALTER TABLE flag_trigger DROP COLUMN environment_namespace;
+ALTER TABLE goal DROP COLUMN environment_namespace;
+ALTER TABLE mau DROP COLUMN environment_namespace;
+ALTER TABLE ops_count DROP COLUMN environment_namespace;
+ALTER TABLE ops_progressive_rollout DROP COLUMN environment_namespace;
+ALTER TABLE push DROP COLUMN environment_namespace;
+ALTER TABLE segment DROP COLUMN environment_namespace;
+ALTER TABLE segment_user DROP COLUMN environment_namespace;
+ALTER TABLE subscription DROP COLUMN environment_namespace;
+ALTER TABLE tag DROP COLUMN environment_namespace;
