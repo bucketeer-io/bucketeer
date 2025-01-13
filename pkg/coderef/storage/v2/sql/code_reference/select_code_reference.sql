b7b0dfaee07baaeb5b0e6953b4fc40ee5cc45dda
Cyka <1354250064mdzz@gmail.com>
2025-01-13 12:34:59 +0800

    refactor: update repository_type handling in SQL queries to use CASE statement for better clarity

--- pkg/coderef/storage/v2/sql/code_reference/select_code_reference.sql
+++ pkg/coderef/storage/v2/sql/code_reference/select_code_reference.sql
@@ -8,7 +8,13 @@ SELECT
     aliases,
     repository_name,
     repository_owner,
-    repository_type,
+    CASE repository_type
+        WHEN 'GITHUB' THEN 1
+        WHEN 'GITLAB' THEN 2
+        WHEN 'BITBUCKET' THEN 3
+        WHEN 'CUSTOM' THEN 4
+        ELSE 0
+    END as repository_type,
     repository_branch,
     commit_hash,
     environment_id,
