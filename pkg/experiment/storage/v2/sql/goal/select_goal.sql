6260c77d839a9585b6593ed1d6ae85256513637b
Hien Vu <112093533+hvn2k1@users.noreply.github.com>
2025-01-17 15:41:52 +0700

    fix query in select goal api

--- pkg/experiment/storage/v2/sql/goal/select_goal.sql
+++ pkg/experiment/storage/v2/sql/goal/select_goal.sql
@@ -1,12 +1,12 @@
 SELECT
-    id,
-    name,
-    description,
-    connection_type,
-    archived,
-    deleted,
-    created_at,
-    updated_at,
+    goal.id,
+    goal.name,
+    goal.description,
+    goal.connection_type,
+    goal.archived,
+    goal.deleted,
+    goal.created_at,
+    goal.updated_at,
     CASE
         WHEN (
             SELECT
@@ -15,13 +15,13 @@ SELECT
                 experiment ex1
             WHERE
                 ex1.environment_id = ? AND
-                ex1.goal_ids LIKE concat("%", goal.id, "%")
+                ex1.goal_ids LIKE concat("%%", goal.id, "%%")
             ) > 0 THEN TRUE
         ELSE FALSE
     END AS is_in_use_status,
     (
         select
-            CONCAT('[', GROUP_CONCAT(JSON_OBJECT('id', ex2.id, 'name', ex2.name)), ']')
+            JSON_ARRAYAGG(JSON_OBJECT('id', ex2.id, 'name', ex2.name))
         from experiment ex2
         where json_contains(ex2.goal_ids, concat('"', goal.id, '"'), '$')
     ) as experiments
