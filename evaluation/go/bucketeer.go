bc5fd99a5acab7692a7ef53d562f8b722a084a20
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-02-20 15:22:44 +0900

    test: add unit test for go module and update comments
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- evaluation/go/bucketeer.go
+++ evaluation/go/bucketeer.go
@@ -24,26 +24,27 @@ type bucketeer struct{}
 
 // Calculate the input hash of the target property and map it to a float64 between [0,1]
 func (b *bucketeer) bucket(input string) float64 {
-	// Compute MurmurHash3 (128-bit) hash.
-	// The high and low parts of the hash are returned in big-endian format,
-	// which is important for consistency with the Node.js implementation that also uses big-endian.
-	high, low := murmur3.Sum128([]byte(input))
-
-	// Convert to float64 using both 64-bit parts (high and low).
-	// This maintains the full 128-bit range, ensuring a normalized value between [0, 1].
+	high, low := b.murmur128(input)
 	return b.toFloat64(high, low)
 }
 
-// Convert a 128-bit hash (two uint64 values) to a float64 in the range [0,1]
+// Convert a 128-bit hash (two uint64 values) to a float64 in the range [0,1].
 // Because we bucket millions of users, we use the full 128-bit hash to avoid collisions.
 func (*bucketeer) toFloat64(high, low uint64) float64 {
 	// Combine the high and low parts into a single floating-point number.
-	// This utilizes the full 128-bit range for precision.
+	// This maintains the full 128-bit range, ensuring a normalized value between [0,1].
 	full := (float64(high) * math.Pow(2, 64)) + float64(low)
 
 	// Calculate the maximum value for a 128-bit number.
-	maxValue := math.Pow(2, 128) - 1 // Maximum value for 128-bit
+	maxValue := math.Pow(2, 128) - 1
 
 	// Normalize the combined value to the range [0,1].
 	return full / maxValue
+}
+
+// Computes MurmurHash3 (128-bit) hash and returns the high and low parts.
+// The high and low parts of the hash are returned in big-endian format,
+// which is crucial for consistency with the Node.js implementation that also uses big-endian.
+func (b *bucketeer) murmur128(input string) (uint64, uint64) {
+	return murmur3.Sum128([]byte(input))
 }
--- evaluation/typescript/src/bucketeer.ts
+++ evaluation/typescript/src/bucketeer.ts
@@ -7,12 +7,11 @@ export default class Bucketeer {
     return this.toFloat64(high, low);
   }
 
+  // Computes MurmurHash3 (128-bit) hash and returns the high and low parts.
+  // The high and low parts of the hash are returned in big-endian format,
+  // which is crucial for consistency with the Go implementation that also uses big-endian.
   private murmur128(input: string): { high: bigint; low: bigint } {
-    // Compute the MurmurHash3 (128-bit) hash using the native library.
-    // By default, murmurHash3.x64.hash128 returns the hash in big-endian format,
-    // which is crucial for consistency with the Go implementation that also uses big-endian.
     const hash = murmurHash3.x64.hash128(input);
-
     // Convert the hash from hexadecimal to BigInt.
     // The first 16 characters represent the high part (most significant bits),
     // and the last 16 characters represent the low part (least significant bits).
@@ -22,13 +21,18 @@ export default class Bucketeer {
     return { high, low };
   }
 
+  // Convert a 128-bit hash (two uint64 values) to a float64 in the range [0,1].
+  // Because we bucket millions of users, we use the full 128-bit hash to avoid collisions.
   private toFloat64(high: bigint, low: bigint): number {
     // Combine the high and low parts into a single floating-point number.
     // This maintains the full 128-bit range, ensuring a normalized value between [0,1].
     const full = Number(high) * Math.pow(2, 64) + Number(low);
-    const maxValue = Math.pow(2, 128) - 1; // Maximum value for 128-bit
 
-    return full / maxValue; // Normalize to the range [0,1]
+    // Calculate the maximum value for a 128-bit number.
+    const maxValue = Math.pow(2, 128) - 1;
+
+    // Normalize the combined value to the range [0,1].
+    return full / maxValue;
   }
 }
 
