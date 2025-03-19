3b6eae26d7e7fbfb08da9add5fe3a065070fcd34
kakcy <yokeyphone@gmail.com>
2025-03-19 11:47:48 +0900

    refactor: fix test

--- pkg/account/storage/v2/admin_account_test.go
+++ pkg/account/storage/v2/admin_account_test.go
@@ -14,74 +14,74 @@
 
 package v2
 
-// func TestGetAdminAccountV2(t *testing.T) {
-// 	t.Parallel()
-// 	mockController := gomock.NewController(t)
-// 	defer mockController.Finish()
-// 	patterns := []struct {
-// 		desc        string
-// 		setup       func(*accountStorage)
-// 		email       string
-// 		expectedErr error
-// 	}{
-// 		{
-// 			desc: "ErrAdminAccountNotFound",
-// 			setup: func(s *accountStorage) {
-// 				qe := mock.NewMockQueryExecer(mockController)
-// 				s.client.(*mock.MockClient).EXPECT().Qe(
-// 					gomock.Any(),
-// 				).Return(qe)
-// 				row := mock.NewMockRow(mockController)
-// 				row.EXPECT().Scan(gomock.Any()).Return(mysql.ErrNoRows)
-// 				qe.EXPECT().QueryRowContext(
-// 					gomock.Any(), gomock.Any(), gomock.Any(),
-// 				).Return(row)
-// 			},
-// 			email:       "bucketeer@example.com",
-// 			expectedErr: ErrSystemAdminAccountNotFound,
-// 		},
-// 		{
-// 			desc: "Error",
-// 			setup: func(s *accountStorage) {
-// 				qe := mock.NewMockQueryExecer(mockController)
-// 				s.client.(*mock.MockClient).EXPECT().Qe(
-// 					gomock.Any(),
-// 				).Return(qe)
-// 				row := mock.NewMockRow(mockController)
-// 				row.EXPECT().Scan(gomock.Any()).Return(errors.New("error"))
-// 				qe.EXPECT().QueryRowContext(
-// 					gomock.Any(), gomock.Any(), gomock.Any(),
-// 				).Return(row)
+import (
+	"context"
+	"errors"
+	"testing"
 
-// 			},
-// 			email:       "bucketeer@example.com",
-// 			expectedErr: errors.New("error"),
-// 		},
-// 		{
-// 			desc: "Success",
-// 			setup: func(s *accountStorage) {
-// 				qe := mock.NewMockQueryExecer(mockController)
-// 				s.client.(*mock.MockClient).EXPECT().Qe(
-// 					gomock.Any(),
-// 				).Return(qe)
-// 				row := mock.NewMockRow(mockController)
-// 				row.EXPECT().Scan(gomock.Any()).Return(nil)
-// 				qe.EXPECT().QueryRowContext(
-// 					gomock.Any(), gomock.Any(), gomock.Any(),
-// 				).Return(row)
-// 			},
-// 			email:       "bucketeer@example.com",
-// 			expectedErr: nil,
-// 		},
-// 	}
-// 	for _, p := range patterns {
-// 		t.Run(p.desc, func(t *testing.T) {
-// 			storage := newAccountStorageWithMock(t, mockController)
-// 			if p.setup != nil {
-// 				p.setup(storage)
-// 			}
-// 			_, err := storage.GetSystemAdminAccountV2(context.Background(), p.email)
-// 			assert.Equal(t, p.expectedErr, err)
-// 		})
-// 	}
-// }
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+
+	"github.com/bucketeer-io/bucketeer/pkg/storage/v2/mysql"
+	"github.com/bucketeer-io/bucketeer/pkg/storage/v2/mysql/mock"
+)
+
+func TestGetAdminAccountV2(t *testing.T) {
+	t.Parallel()
+	mockController := gomock.NewController(t)
+	defer mockController.Finish()
+	patterns := []struct {
+		desc        string
+		setup       func(*accountStorage)
+		email       string
+		expectedErr error
+	}{
+		{
+			desc: "ErrAdminAccountNotFound",
+			setup: func(s *accountStorage) {
+				row := mock.NewMockRow(mockController)
+				row.EXPECT().Scan(gomock.Any()).Return(mysql.ErrNoRows)
+				s.qe.(*mock.MockQueryExecer).EXPECT().QueryRowContext(
+					gomock.Any(), gomock.Any(), gomock.Any(),
+				).Return(row)
+			},
+			email:       "bucketeer@example.com",
+			expectedErr: ErrSystemAdminAccountNotFound,
+		},
+		{
+			desc: "Error",
+			setup: func(s *accountStorage) {
+				row := mock.NewMockRow(mockController)
+				row.EXPECT().Scan(gomock.Any()).Return(errors.New("error"))
+				s.qe.(*mock.MockQueryExecer).EXPECT().QueryRowContext(
+					gomock.Any(), gomock.Any(), gomock.Any(),
+				).Return(row)
+
+			},
+			email:       "bucketeer@example.com",
+			expectedErr: errors.New("error"),
+		},
+		{
+			desc: "Success",
+			setup: func(s *accountStorage) {
+				row := mock.NewMockRow(mockController)
+				row.EXPECT().Scan(gomock.Any()).Return(nil)
+				s.qe.(*mock.MockQueryExecer).EXPECT().QueryRowContext(
+					gomock.Any(), gomock.Any(), gomock.Any(),
+				).Return(row)
+			},
+			email:       "bucketeer@example.com",
+			expectedErr: nil,
+		},
+	}
+	for _, p := range patterns {
+		t.Run(p.desc, func(t *testing.T) {
+			storage := newAccountStorageWithMock(t, mockController)
+			if p.setup != nil {
+				p.setup(storage)
+			}
+			_, err := storage.GetSystemAdminAccountV2(context.Background(), p.email)
+			assert.Equal(t, p.expectedErr, err)
+		})
+	}
+}
