c66a23eceb7d5973293ab0437c1fad9fd6cbbcd3
steveninhle <lephuocninh7118@gmail.com>
2025-03-28 12:00:18 +0700

    fix: showing total count on the bottom list page

--- ui/dashboard/src/pages/notifications/collection-loader/index.tsx
+++ ui/dashboard/src/pages/notifications/collection-loader/index.tsx
@@ -1,6 +1,5 @@
 import { SortingState } from '@tanstack/react-table';
 import { getCurrentEnvironment, useAuth } from 'auth';
-import { LIST_PAGE_SIZE } from 'constants/app';
 import { sortingListFields } from 'constants/collection';
 import { Notification } from '@types';
 import Pagination from 'components/pagination';
@@ -72,7 +71,7 @@ const CollectionLoader = ({
         onSortingChange={onSortingChangeHandler}
         emptyCollection={emptyState}
       />
-      {totalCount > LIST_PAGE_SIZE && !isLoading && (
+      {!isLoading && (
         <Pagination
           page={filters.page}
           totalCount={totalCount}
