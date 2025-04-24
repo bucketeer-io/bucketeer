3fa7aefa84e8bf2beeca26ef4ce719405899da8e
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-04-24 15:13:47 +0900

    chore: run evaluation in real time when linking goal event
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/subscriber/processor/goal_events_dwh.go
+++ pkg/subscriber/processor/goal_events_dwh.go
@@ -246,7 +246,7 @@ func (w *goalEvtWriter) linkGoalEvent(
 	environmentId, tag string,
 	experiments []*exproto.Experiment,
 ) ([]*featureproto.Evaluation, bool, error) {
-	evalExp, retriable, err := w.linkGoalEventByExperimentByBigQuery(ctx, event, environmentId, tag, experiments)
+	evalExp, retriable, err := w.linkGoalEventByExperiment(ctx, event, environmentId, tag, experiments)
 	if err != nil {
 		return nil, retriable, err
 	}
