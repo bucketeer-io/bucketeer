7f23d947b75a237bc5efec9df72081d9b2aeca81
steveninhle <lephuocninh7118@gmail.com>
2025-05-07 15:12:01 +0700

    feat: update feature flag details variations page

--- ui/dashboard/src/pages/feature-flag-details/variation/index.tsx
+++ ui/dashboard/src/pages/feature-flag-details/variation/index.tsx
@@ -85,7 +85,7 @@ const Variation = ({ feature }: VariationProps) => {
   }, [feature]);
 
   return (
-    <div className="flex flex-col w-full p-5 pt-0">
+    <div className="p-6 pt-0 w-full min-w-[900px]">
       <FormProvider {...form}>
         <Form onSubmit={form.handleSubmit(onSubmit)}>
           <div className="flex flex-col w-full gap-y-6">
