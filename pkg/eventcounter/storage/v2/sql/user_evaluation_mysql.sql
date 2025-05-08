5e82ec9775eaa8ca5e5865615e2c7d72649e64a7
Cyka <1354250064mdzz@gmail.com>
2025-05-08 11:26:18 +0800

    feat: implement user evaluation query in MySQL event storage

---
+++ pkg/eventcounter/storage/v2/sql/user_evaluation_mysql.sql
@@ -0,0 +1,18 @@
+SELECT
+    user_id as userID,
+    feature_id as featureID,
+    feature_version as featureVersion,
+    variation_id as variationID,
+    reason,
+    UNIX_TIMESTAMP(timestamp) as timestamp
+FROM
+    evaluation_event
+WHERE
+    environment_id = ?
+    AND feature_id = ?
+    AND feature_version = ?
+    AND user_id = ?
+    AND timestamp BETWEEN ? AND ?
+ORDER BY
+    timestamp DESC
+LIMIT 1; 
