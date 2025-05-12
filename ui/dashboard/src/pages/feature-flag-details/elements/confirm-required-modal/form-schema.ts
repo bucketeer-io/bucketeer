b52a867e7a9fa3fe6099384759ac7b77244786aa
steveninhle <lephuocninh7118@gmail.com>
2025-05-12 16:31:43 +0700

    feat: implement flag details targeting page

---
+++ ui/dashboard/src/pages/feature-flag-details/elements/confirm-required-modal/form-schema.ts
@@ -0,0 +1,33 @@
+import { i18n } from 'i18n';
+import * as yup from 'yup';
+
+const translation = i18n.t;
+const requiredMessage = translation('message:required-field');
+
+export const formSchema = yup.object().shape({
+  requireComment: yup.boolean(),
+  resetSampling: yup.boolean(),
+  comment: yup.string().when('requireComment', {
+    is: (requireComment: boolean) => requireComment,
+    then: schema => schema.required(requiredMessage)
+  }),
+  scheduleType: yup.string(),
+  scheduleAt: yup.string().test('validate', function (value, context) {
+    const scheduleType = context.from && context.from[0].value.scheduleType;
+    if (scheduleType === 'SCHEDULE') {
+      if (!value)
+        return context.createError({
+          message: requiredMessage,
+          path: context.path
+        });
+      if (+value * 1000 < new Date().getTime())
+        return context.createError({
+          message: translation(
+            'message:validation.operation.later-than-current-time'
+          ),
+          path: context.path
+        });
+    }
+    return true;
+  })
+});
