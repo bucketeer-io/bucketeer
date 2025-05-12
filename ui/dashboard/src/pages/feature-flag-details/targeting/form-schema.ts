f3d38644fdc62049ecffa2b793e19f7a0896ec82
steveninhle <lephuocninh7118@gmail.com>
2025-05-12 16:31:43 +0700

    feat: implement flag details targeting page

---
+++ ui/dashboard/src/pages/feature-flag-details/targeting/form-schema.ts
@@ -0,0 +1,127 @@
+import { i18n } from 'i18n';
+import * as yup from 'yup';
+import { StrategyType } from '@types';
+import { RuleClauseType } from './types';
+
+export type RuleClauseSchema = yup.InferType<typeof ruleClauseSchema>;
+const translation = i18n.t;
+const requiredMessage = translation('message:required-field');
+
+const ruleClauseSchema = yup.object().shape({
+  id: yup.string().required(),
+  type: yup
+    .string()
+    .oneOf([
+      RuleClauseType.COMPARE,
+      RuleClauseType.SEGMENT,
+      RuleClauseType.DATE,
+      RuleClauseType.FEATURE_FLAG
+    ])
+    .required(requiredMessage),
+  attribute: yup.string().when('type', {
+    is: (type: string) => type === RuleClauseType.SEGMENT,
+    then: schema => schema,
+    otherwise: schema => schema.required(requiredMessage)
+  }),
+  operator: yup.string().required(requiredMessage),
+  values: yup
+    .array()
+    .of(yup.string())
+    .min(1, requiredMessage)
+    .required(requiredMessage)
+});
+
+const strategySchema = yup.object().shape({
+  currentOption: yup.string(),
+  type: yup
+    .string()
+    .oneOf([StrategyType.FIXED, StrategyType.ROLLOUT])
+    .required(requiredMessage),
+  fixedStrategy: yup
+    .object()
+    .shape({
+      variation: yup.string()
+    })
+    .when('type', {
+      is: (type: string) => type === StrategyType.ROLLOUT,
+      then: schema => schema,
+      otherwise: schema => schema.required(requiredMessage)
+    }),
+  rolloutStrategy: yup
+    .array()
+    .of(
+      yup.object().shape({
+        variation: yup.string().required(requiredMessage),
+        weight: yup.number().required(requiredMessage)
+      })
+    )
+    .when('type', {
+      is: (type: string) => type === StrategyType.FIXED,
+      then: schema => schema,
+      otherwise: schema => schema.required(requiredMessage)
+    })
+    .test('sum', (value, context) => {
+      if (context.parent?.type !== StrategyType.ROLLOUT) {
+        return true;
+      }
+      if (value) {
+        const total = value
+          .map(v => Number(v.weight))
+          .reduce((total, current) => {
+            return total + (current || 0);
+          }, 0);
+        if (total !== 100)
+          return context.createError({
+            message: translation('message:validation.should-be-percent'),
+            path: context.path
+          });
+      }
+      return true;
+    })
+});
+export type StrategySchema = yup.InferType<typeof strategySchema>;
+
+export const rulesSchema = yup.object().shape({
+  id: yup.string(),
+  clauses: yup.array().of(ruleClauseSchema).required(),
+  strategy: strategySchema
+});
+
+export type RuleSchema = yup.InferType<typeof rulesSchema>;
+
+export const formSchema = yup.object().shape({
+  prerequisites: yup.array().of(
+    yup.object().shape({
+      featureId: yup.string().required(requiredMessage),
+      variationId: yup.string().required(requiredMessage)
+    })
+  ),
+  targetIndividualRules: yup
+    .array()
+    .required()
+    .of(
+      yup.object().shape({
+        variationId: yup.string().required(requiredMessage),
+        name: yup.string(),
+        users: yup
+          .array()
+          .required(requiredMessage)
+          .test('required', (value, context) => {
+            if ((Array.isArray(value) && !value.length) || !value) {
+              return context.createError({
+                message: requiredMessage,
+                path: context.path
+              });
+            }
+
+            return true;
+          })
+      })
+    ),
+  rules: yup.array().of(rulesSchema),
+  defaultStrategy: strategySchema,
+  enabled: yup.boolean().required(requiredMessage),
+  isShowRules: yup.boolean().required(requiredMessage)
+});
+
+export type TargetingSchema = yup.InferType<typeof formSchema>;
