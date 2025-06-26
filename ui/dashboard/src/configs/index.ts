a7d09d1cd9f5bdd9b3d42a2e09c67a0f5a87f03a
steveninhle <lephuocninh7118@gmail.com>
2025-06-26 17:14:51 +0700

    fix: update dashboard ui issues

--- ui/dashboard/src/configs/index.ts
+++ ui/dashboard/src/configs/index.ts
@@ -12,7 +12,7 @@ declare global {
       DEMO_SIGN_IN_EMAIL?: string;
       DEMO_SIGN_IN_PASSWORD?: string;
       GOOGLE_TAG_MANAGER_ID?: string;
-      SDK_API_ENDPOINT?: string;
+      API_ENDPOINT?: string;
     };
   }
 }
@@ -27,10 +27,10 @@ export const urls = {
     releaseMode !== 'prod'
       ? `${import.meta.env.VITE_AUTH_REDIRECT_ENDPOINT}/`
       : `${window.location.origin}/v3/`, // TODO: Remove the `/v3` when the new console is released
-  SDK_API_ENDPOINT:
+  API_ENDPOINT:
     releaseMode !== 'prod'
-      ? import.meta.env.VITE_SDK_API_ENDPOINT
-      : window.env?.SDK_API_ENDPOINT
+      ? import.meta.env.VITE_API_ENDPOINT
+      : window.env?.API_ENDPOINT
 };
 
 export const GOOGLE_TAG_MANAGER_ID = window.env?.GOOGLE_TAG_MANAGER_ID || '';
--- ui/dashboard/src/elements/page-header/support/index.tsx
+++ ui/dashboard/src/elements/page-header/support/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import {
   IconHelpOutlineOutlined,
   IconLaunchOutlined
@@ -5,7 +6,11 @@ import {
 import { Link } from 'react-router-dom';
 import { urls } from 'configs';
 import { DOCUMENTATION_LINKS } from 'constants/documentation-links';
+import { useToast } from 'hooks';
 import { useTranslation } from 'i18n';
+import { copyToClipBoard } from 'utils/function';
+import { IconCopy } from '@icons';
+import Button from 'components/button';
 import Divider from 'components/divider';
 import Icon from 'components/icon';
 import { Popover } from 'components/popover';
@@ -14,7 +19,7 @@ const SupportPopoverItem = ({ to, title }: { to: string; title: string }) => (
   <Link
     to={to}
     target="_blank"
-    className="flex items-center w-full gap-x-2 px-3 py-1 typo-para-small text-gray-700 hover:bg-gray-200 hover:text-primary-500 hover:underline transition-colors rounded"
+    className="flex items-center w-full gap-x-2 px-8 py-1 typo-para-small text-gray-700 hover:bg-gray-200 hover:text-primary-500 hover:underline transition-colors rounded"
   >
     {title}
     <Icon icon={IconLaunchOutlined} size="xxs" />
@@ -22,10 +27,20 @@ const SupportPopoverItem = ({ to, title }: { to: string; title: string }) => (
 );
 
 const SupportPopover = () => {
-  const { t } = useTranslation(['form']);
+  const { t } = useTranslation(['form', 'message', 'common']);
+  const { notify } = useToast();
+
+  const handleCopy = useCallback(() => {
+    copyToClipBoard(urls.API_ENDPOINT || '');
+    notify({
+      message: t('message:copied')
+    });
+  }, [urls]);
+
   return (
     <Popover
       align="end"
+      sideOffset={6}
       className="py-1.5 shadow-card-secondary"
       trigger={
         <div className="flex-center size-fit">
@@ -34,19 +49,37 @@ const SupportPopover = () => {
       }
     >
       <div className="flex flex-col w-full gap-y-0.5">
-        <div className="flex flex-col w-full gap-y-0.5 px-3 py-1 typo-para-small">
-          <p className="text-gray-700">{t('sdk-api-endpoint')}</p>
-          <p className="text-gray-600">{urls.SDK_API_ENDPOINT || ''}</p>
+        {urls.API_ENDPOINT && (
+          <>
+            <div className="flex flex-col w-full gap-y-0.5 px-3 py-1 typo-para-small">
+              <p className="text-gray-700">{t('sdk-api-endpoint')}</p>
+              <div className="flex items-center gap-x-1.5">
+                <p className="text-gray-600">{urls.API_ENDPOINT || ''}</p>
+                <Button
+                  variant="grey"
+                  className="size-fit flex-center"
+                  onClick={handleCopy}
+                >
+                  <Icon icon={IconCopy} size="sm" />
+                </Button>
+              </div>
+            </div>
+            <Divider />
+          </>
+        )}
+        <div className="px-3">
+          <p className="typo-para-small font-semibold text-gray-700 pt-1">
+            {t('common:docs')}
+          </p>
+          <SupportPopoverItem
+            to={DOCUMENTATION_LINKS.GETTING_STARTED}
+            title={t('getting-started')}
+          />
+          <SupportPopoverItem
+            to={DOCUMENTATION_LINKS.SDK_CONFIGURATION}
+            title={t('sdk-configuration')}
+          />
         </div>
-        <Divider />
-        <SupportPopoverItem
-          to={DOCUMENTATION_LINKS.GETTING_STARTED}
-          title={t('getting-started')}
-        />
-        <SupportPopoverItem
-          to={DOCUMENTATION_LINKS.SDK_CONFIGURATION}
-          title={t('sdk-configuration')}
-        />
       </div>
     </Popover>
   );
