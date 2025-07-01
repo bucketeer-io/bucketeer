cd52fd1ce0d595357d6f2528076949da653bc31c
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-07-01 12:26:37 +0900

    chore(ui/dashboard): change pagination display format (#1904)
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- ui/dashboard/src/components/pagination/pagination-count.tsx
+++ ui/dashboard/src/components/pagination/pagination-count.tsx
@@ -2,15 +2,21 @@ import { useTranslation } from 'i18n';
 
 export type PaginationCountProps = {
   totalItems: number;
-  value?: number;
+  startItem: number;
+  endItem: number;
 };
 
-const PaginationCount = ({ totalItems, value = 0 }: PaginationCountProps) => {
+const PaginationCount = ({
+  totalItems,
+  startItem,
+  endItem
+}: PaginationCountProps) => {
   const { t } = useTranslation(['common']);
   return (
     <p className="text-gray-600 typo-para-medium">
       {t('pagination-count', {
-        count: value,
+        start: startItem,
+        end: endItem,
         total: totalItems
       })}
     </p>
