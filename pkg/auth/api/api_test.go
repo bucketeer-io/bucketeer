1458b3530b6ef9b98d2e2be7b233446f2be4e67f
Hien Vu <112093533+hvn2k1@users.noreply.github.com>
2025-07-14 13:46:13 +0900

    Update api_test.go

--- pkg/auth/api/api_test.go
+++ pkg/auth/api/api_test.go
@@ -13,3 +13,62 @@
 // limitations under the License.
 
 package api
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	authproto "github.com/bucketeer-io/bucketeer/proto/auth"
+)
+
+func TestAuthService_GetDeploymentStatus(t *testing.T) {
+	t.Parallel()
+	patterns := []struct {
+		desc        string
+		setup       func()
+		expectedErr error
+		expected    *authproto.GetDeploymentStatusResponse
+	}{
+		{
+			desc: "success: true",
+			setup: func() {
+				err := os.Setenv("DEMO_SITE_ENABLED", "true")
+				if err != nil {
+					t.Fatalf("failed to set environment variable: %v", err)
+				}
+			},
+			expectedErr: nil,
+			expected: &authproto.GetDeploymentStatusResponse{
+				IsDemoSiteEnabled: true,
+			},
+		},
+		{
+			desc: "success: false",
+			setup: func() {
+				err := os.Setenv("DEMO_SITE_ENABLED", "false")
+				if err != nil {
+					t.Fatalf("failed to set environment variable: %v", err)
+				}
+			},
+			expectedErr: nil,
+			expected: &authproto.GetDeploymentStatusResponse{
+				IsDemoSiteEnabled: false,
+			},
+		},
+	}
+	for _, p := range patterns {
+		t.Run(p.desc, func(t *testing.T) {
+			p.setup()
+			options := defaultOptions
+			s := &authService{
+				logger: options.logger,
+			}
+			resp, err := s.GetDeploymentStatus(context.Background(), nil)
+			assert.Equal(t, p.expectedErr, err)
+			assert.Equal(t, p.expected, resp)
+		})
+	}
+}
