d5e032659edc59ead5650c4467edec52e91cdaf0
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-07-17 00:05:10 +0900

    chore: split subquery for faster querying
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/feature/storage/v2/sql/feature/count_features_by_experiment.sql
+++ pkg/feature/storage/v2/sql/feature/count_features_by_experiment.sql
@@ -2,16 +2,6 @@ SELECT
     COUNT(DISTINCT feature.id)
 FROM
     feature
-LEFT OUTER JOIN feature_last_used_info ON
-    feature.id = feature_last_used_info.feature_id AND
-    feature.environment_id = feature_last_used_info.environment_id AND
-    NOT EXISTS (
-        SELECT 1 FROM feature_last_used_info flui2
-        WHERE flui2.feature_id = feature_last_used_info.feature_id
-        AND flui2.environment_id = feature_last_used_info.environment_id
-        AND (flui2.last_used_at > feature_last_used_info.last_used_at
-        OR (flui2.last_used_at = feature_last_used_info.last_used_at AND flui2.version > feature_last_used_info.version))
-    )
 LEFT JOIN experiment ON
     feature.id = experiment.feature_id AND
     feature.environment_id = experiment.environment_id
