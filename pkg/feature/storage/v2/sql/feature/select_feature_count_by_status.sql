574ad3d166640861795231b00571dbaa6e478e6d
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-07-16 23:08:57 +0900

    fix: last used info always shows as never used
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/feature/storage/v2/sql/feature/select_feature_count_by_status.sql
+++ pkg/feature/storage/v2/sql/feature/select_feature_count_by_status.sql
@@ -1,13 +1,19 @@
 SELECT
     COUNT(DISTINCT feature.id) AS total_features,
-    COUNT(DISTINCT CASE WHEN FROM_UNIXTIME(last_used_at) >= DATE_SUB(now(), INTERVAL 7 DAY) THEN feature.id END) AS active,
-    COUNT(DISTINCT CASE WHEN FROM_UNIXTIME(last_used_at) < DATE_SUB(now(), INTERVAL 7 DAY) THEN feature.id END) AS inactive
+    COUNT(DISTINCT CASE WHEN FROM_UNIXTIME(feature_last_used_info.last_used_at) >= DATE_SUB(now(), INTERVAL 7 DAY) THEN feature.id END) AS active,
+    COUNT(DISTINCT CASE WHEN FROM_UNIXTIME(feature_last_used_info.last_used_at) < DATE_SUB(now(), INTERVAL 7 DAY) THEN feature.id END) AS inactive
 FROM
     feature
 LEFT JOIN feature_last_used_info ON
     feature.id = feature_last_used_info.feature_id AND
     feature.environment_id = feature_last_used_info.environment_id AND
-    feature.version = feature_last_used_info.version
+    NOT EXISTS (
+        SELECT 1 FROM feature_last_used_info flui2
+        WHERE flui2.feature_id = feature_last_used_info.feature_id
+        AND flui2.environment_id = feature_last_used_info.environment_id
+        AND (flui2.last_used_at > feature_last_used_info.last_used_at
+        OR (flui2.last_used_at = feature_last_used_info.last_used_at AND flui2.version > feature_last_used_info.version))
+    )
 WHERE
     feature.archived = 0 AND
     feature.deleted = 0 AND
