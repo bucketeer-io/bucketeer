b687603cc05abc0b82337590352272dd6878e085
Hien Vu <112093533+hvn2k1@users.noreply.github.com>
2025-07-23 13:42:39 +0900

    fix: get organization not return enough information

--- pkg/environment/storage/v2/sql/organization/select_organization.sql
+++ pkg/environment/storage/v2/sql/organization/select_organization.sql
@@ -1,16 +1,34 @@
 SELECT
-    id,
-    name,
-    owner_email,
-    url_code,
-    description,
-    disabled,
-    archived,
-    trial,
-    system_admin,
-    created_at,
-    updated_at
+    organization.id,
+    organization.name,
+    organization.owner_email,
+    organization.url_code,
+    organization.description,
+    organization.disabled,
+    organization.archived,
+    organization.trial,
+    organization.system_admin,
+    organization.created_at,
+    organization.updated_at,
+    COUNT(DISTINCT project.id) AS projects,
+    COUNT(DISTINCT environment_v2.id) AS environments,
+    COUNT(DISTINCT account_v2.email) AS users
 FROM
     organization
+LEFT JOIN project ON organization.id = project.organization_id
+LEFT JOIN environment_v2 ON organization.id = environment_v2.organization_id
+LEFT JOIN account_v2 ON organization.id = account_v2.organization_id
 WHERE
-    id = ?
+    organization.id = ?
+GROUP BY
+    organization.id,
+    organization.name,
+    organization.owner_email,
+    organization.url_code,
+    organization.description,
+    organization.disabled,
+    organization.archived,
+    organization.trial,
+    organization.system_admin,
+    organization.created_at,
+    organization.updated_at
