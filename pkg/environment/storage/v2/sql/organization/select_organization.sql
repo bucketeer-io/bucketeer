206aa0b28e00160a52ccb912d047f95e1e2b282d
Hien Vu <112093533+hvn2k1@users.noreply.github.com>
2025-07-23 17:24:10 +0900

    Update select_organization.sql

--- pkg/environment/storage/v2/sql/organization/select_organization.sql
+++ pkg/environment/storage/v2/sql/organization/select_organization.sql
@@ -10,25 +10,8 @@ SELECT
     organization.system_admin,
     organization.created_at,
     organization.updated_at,
-    COUNT(DISTINCT project.id) AS projects,
-    COUNT(DISTINCT environment_v2.id) AS environments,
-    COUNT(DISTINCT account_v2.email) AS users
-FROM
-    organization
-LEFT JOIN project ON organization.id = project.organization_id
-LEFT JOIN environment_v2 ON organization.id = environment_v2.organization_id
-LEFT JOIN account_v2 ON organization.id = account_v2.organization_id
-WHERE
-    organization.id = ?
-GROUP BY
-    organization.id,
-    organization.name,
-    organization.owner_email,
-    organization.url_code,
-    organization.description,
-    organization.disabled,
-    organization.archived,
-    organization.trial,
-    organization.system_admin,
-    organization.created_at,
-    organization.updated_at
+    (SELECT COUNT(DISTINCT id) FROM project WHERE organization_id = organization.id) AS projects,
+    (SELECT COUNT(DISTINCT id) FROM environment_v2 WHERE organization_id = organization.id) AS environments,
+    (SELECT COUNT(DISTINCT email) FROM account_v2 WHERE organization_id = organization.id) AS users
+FROM organization
+WHERE organization.id = ?
