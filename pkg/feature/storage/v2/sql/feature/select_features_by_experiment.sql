3bc8b58a3c172b764d165a288a6ac1f6be5cf40f
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-08-04 19:00:03 +0900

    fix: last used info always shows as never used (#2003)
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/feature/storage/v2/sql/feature/select_features_by_experiment.sql
+++ pkg/feature/storage/v2/sql/feature/select_features_by_experiment.sql
@@ -52,10 +52,17 @@ SELECT DISTINCT
     COALESCE(feature_last_used_info.client_latest_version, '') AS client_latest_version
 FROM
     feature
-LEFT OUTER JOIN feature_last_used_info ON
+LEFT JOIN feature_last_used_info ON
     feature.id = feature_last_used_info.feature_id AND
     feature.environment_id = feature_last_used_info.environment_id AND
-    feature.version = feature_last_used_info.version
-JOIN experiment ON
+    NOT EXISTS (
+        SELECT 1 FROM feature_last_used_info flui2
+        WHERE flui2.feature_id = feature_last_used_info.feature_id
+        AND flui2.environment_id = feature_last_used_info.environment_id
+        AND (flui2.last_used_at > feature_last_used_info.last_used_at
+        OR (flui2.last_used_at = feature_last_used_info.last_used_at AND flui2.version > feature_last_used_info.version))
+    )
+LEFT JOIN experiment ON
     feature.id = experiment.feature_id AND
-    feature.environment_id = experiment.environment_id
+    feature.environment_id = experiment.environment_id AND
+    experiment.deleted = 0
