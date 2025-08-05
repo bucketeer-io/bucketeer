3215e21c39b68a5a1fb9c3024f303bab2eabceb1
steveninhle <lephuocninh7118@gmail.com>
2025-08-05 13:11:00 +0700

    feat: update manual rollout flag

--- ui/dashboard/src/pages/feature-flag-details/targeting/segment-rule/strategy.tsx
+++ ui/dashboard/src/pages/feature-flag-details/targeting/segment-rule/strategy.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { Trans } from 'react-i18next';
+import useOptions from 'hooks/use-options';
 import { useTranslation } from 'i18n';
 import { RuleStrategyVariation, StrategyType } from '@types';
 import { cn } from 'utils/style';
-import { IconInfo } from '@icons';
 import ExperimentSelect from 'pages/experiments/experiments-modal/experiment-create-update/define-audience/experiment-select';
 import Divider from 'components/divider';
 import {
@@ -16,47 +16,13 @@ import {
 import Form from 'components/form';
 import Icon from 'components/icon';
 import Input from 'components/input';
+import InputGroup from 'components/input-group';
 import { RadioGroup, RadioGroupItem } from 'components/radio';
-import { Tooltip } from 'components/tooltip';
 import { StrategySchema } from '../form-schema';
 import { isEquallyVariations } from '../utils';
 import PercentageBar from './percentage-bar';
 import PercentageInput from './percentage-input';
 import { VariationOption } from './variation';
-
-const experimentOptions = [
-  {
-    label: '5%',
-    value: 5
-  },
-  {
-    label: '10%',
-    value: 10
-  },
-  {
-    label: '50%',
-    value: 50
-  },
-  {
-    label: '90%',
-    value: 90
-  },
-  {
-    label: 'Custom',
-    value: 'custom'
-  }
-];
-
-const startOptions = [
-  {
-    label: '= Split equally',
-    value: 'equally'
-  },
-  {
-    label: '% Split by percentage',
-    value: 'percentage'
-  }
-];
 
 interface Props {
   rootName: string;
@@ -85,6 +51,7 @@ const Strategy = ({
   const { t } = useTranslation(['table', 'common', 'form', 'message']);
   const { control, watch, setError, setValue, clearErrors, setFocus } =
     useFormContext();
+  const { splitExperimentOptions, audienceTrafficOptions } = useOptions();
 
   const type = watch(`${rootName}.type`);
   const currentOption = watch(`${rootName}.currentOption`);
@@ -161,7 +128,7 @@ const Strategy = ({
   };
 
   useEffect(() => {
-    const isActiveCustomExperiment = !experimentOptions
+    const isActiveCustomExperiment = !audienceTrafficOptions
       .map(i => i.value)
       .includes(experimentPercentage);
 
@@ -181,7 +148,7 @@ const Strategy = ({
   }, []);
 
   return (
-    <div>
+    <div className="px-2">
       {label && (
         <Form.Label
           required={isRequired}
@@ -244,193 +211,183 @@ const Strategy = ({
 
       {isShowPercentage && (
         <>
-          <div className="typo-para-small text-gray-500 my-5">
-            {t('form:experiments.define-audience.any-traffic')}
+          <div className="typo-para-medium text-gray-700 my-5">
+            {t('form:experiments.define-audience.split-experiment-traffic')}
           </div>
 
-          <div className="w-full p-4 bg-gray-additional rounded-lg">
-            <div className="flex flex-col w-full gap-y-4 typo-para-small leading-[14px] text-gray-600">
-              <p>{t('form:experiments.define-audience.audience-amount')}</p>
-              <div className="w-full h-3 p-[1px] border border-gray-400 rounded-full bg-gray-100">
-                <div
-                  className={cn('h-full bg-primary-500 rounded-l-full', {
-                    'rounded-r-full': experimentPercentage >= 100
-                  })}
-                  style={{
-                    width: `${experimentPercentage > 100 ? 100 : experimentPercentage}%`
-                  }}
-                />
+          <div className="flex flex-col w-full gap-y-4 typo-para-small leading-[14px] text-gray-600 mb-6">
+            <p>{t('form:experiments.define-audience.audience-amount')}</p>
+            <div className="w-full h-3 p-[1px] border border-gray-400 rounded-full ">
+              <div
+                className={cn('h-full bg-primary-500 rounded-l-full', {
+                  'rounded-r-full': experimentPercentage >= 100
+                })}
+                style={{
+                  width: `${experimentPercentage > 100 ? 100 : experimentPercentage}%`
+                }}
+              />
+            </div>
+            <div className="flex items-center w-full gap-x-4">
+              <div className="flex items-center gap-x-2">
+                <div className="flex-center size-5 m-0.5 rounded bg-primary-500" />
+                <p>{`${experimentPercentage}% ${t('form:experiments.define-audience.in-this-experiment')}`}</p>
               </div>
-              <div className="flex items-center w-full gap-x-4">
-                <div className="flex items-center gap-x-2">
-                  <div className="flex-center size-5 m-0.5 rounded bg-primary-500" />
-                  <p>{`${experimentPercentage}% ${t('form:experiments.define-audience.in-this-experiment')}`}</p>
-                </div>
-                <div className="flex items-center gap-x-2">
-                  <div className="flex-center size-5 m-0.5 border border-gray-400 rounded bg-gray-100" />
-                  <p>{`${100 - experimentPercentage}% ${t('form:experiments.define-audience.not-in-experiment')}`}</p>
-                </div>
+              <div className="flex items-center gap-x-2">
+                <div className="flex-center size-5 m-0.5 border border-gray-400 rounded bg-gray-100" />
+                <p>{`${100 - experimentPercentage}% ${t('form:experiments.define-audience.not-in-experiment')}`}</p>
               </div>
             </div>
-            <Divider className="my-5 border-gray-300" />
+          </div>
 
-            <Form.Field
-              control={control}
-              name={`${rootName}.${strategyName}.audience.percentage`}
-              render={({ field }) => {
-                return (
-                  <Form.Item className="flex flex-col flex-1 py-0 w-full">
-                    <Form.Control>
-                      <div>
-                        <div className="flex items-center w-full gap-x-2">
-                          <p className="typo-para-medium leading-5 text-gray-600 whitespace-nowrap">
-                            {t(
-                              'form:experiments.define-audience.in-this-experiment'
-                            )}
-                            :
-                          </p>
-                          {experimentOptions.map((item, index) => (
-                            <ExperimentSelect
-                              key={index}
-                              label={item.label}
-                              value={item.value}
-                              isActive={
-                                item.value === 'custom'
-                                  ? isCustomExperiment
-                                  : field.value === item.value
-                              }
-                              onSelect={handleSelectExperiment}
-                            />
-                          ))}
-                        </div>
-                        {isCustomExperiment && (
-                          <Input
-                            {...field}
-                            ref={inputRef}
-                            type="number"
-                            className="mt-5"
-                            value={field.value ?? ''}
-                            onWheel={e => e.currentTarget.blur()}
-                            onChange={value => field.onChange(value)}
+          <Form.Field
+            control={control}
+            name={`${rootName}.${strategyName}.audience.percentage`}
+            render={({ field }) => {
+              return (
+                <Form.Item className="flex flex-col flex-1 py-0 w-full">
+                  <Form.Control>
+                    <div>
+                      <div className="flex items-center w-full gap-x-2">
+                        <p className="typo-para-medium leading-5 text-gray-600 whitespace-nowrap">
+                          {t(
+                            'form:experiments.define-audience.in-this-experiment'
+                          )}
+                          :
+                        </p>
+                        {audienceTrafficOptions.map((item, index) => (
+                          <ExperimentSelect
+                            key={index}
+                            label={item.label}
+                            value={item.value}
+                            isActive={
+                              item.value === 'custom'
+                                ? isCustomExperiment
+                                : field.value === item.value
+                            }
+                            onSelect={handleSelectExperiment}
                           />
+                        ))}
+                        {isCustomExperiment && (
+                          <div className="flex-1 relative">
+                            <InputGroup
+                              addon={'%'}
+                              addonSlot="right"
+                              className="w-32 overflow-hidden"
+                              addonClassName="top-[1px] bottom-[1px] right-[1px] translate-x-0 translate-y-0 !flex-center rounded-r-lg bg-gray-200 w-[29px] typo-para-medium text-gray-700"
+                            >
+                              <Input
+                                {...field}
+                                ref={inputRef}
+                                value={field.value ?? ''}
+                                onWheel={e => e.currentTarget.blur()}
+                                onChange={value => field.onChange(value)}
+                                type="number"
+                                className="text-right pl-[5px]"
+                              />
+                            </InputGroup>
+                          </div>
                         )}
                       </div>
-                    </Form.Control>
-                    <Form.Message />
-                  </Form.Item>
-                );
+                    </div>
+                  </Form.Control>
+                  <Form.Message />
+                </Form.Item>
+              );
+            }}
+          />
+
+          <div className="flex items-center w-full gap-x-2 mt-4 typo-para-medium leading-5 text-gray-600 whitespace-nowrap">
+            <Trans
+              i18nKey={
+                'form:experiments.define-audience.not-in-experiment-served'
+              }
+              values={{
+                percent: `${100 - experimentPercentage}%`
+              }}
+              components={{
+                highlight: (
+                  <div className="flex-center size-fit p-3 rounded-lg typo-para-medium leading-5 text-gray-700 bg-gray-additional-2" />
+                )
               }}
             />
+            <div className="flex-1">
+              <Form.Field
+                control={control}
+                name={`${rootName}.${strategyName}.audience.defaultVariation`}
+                render={({ field }) => {
+                  const options = variationOptions.slice(0, -1);
+                  const option = options.find(
+                    item => item.value === field.value
+                  );
 
-            <div className="flex items-center w-full gap-x-2 mt-4 typo-para-medium leading-5 text-gray-600 whitespace-nowrap">
-              <Trans
-                i18nKey={
-                  'form:experiments.define-audience.not-in-experiment-served'
-                }
-                values={{
-                  percent: `${100 - experimentPercentage}%`
-                }}
-                components={{
-                  highlight: (
-                    <div className="flex-center size-fit p-3 rounded-lg typo-para-medium leading-5 text-gray-700 bg-gray-additional-2" />
-                  )
-                }}
-              />
-              <div className="flex-1">
-                <Form.Field
-                  control={control}
-                  name={`${rootName}.${strategyName}.audience.defaultVariation`}
-                  render={({ field }) => {
-                    const options = variationOptions.slice(0, -1);
-                    const option = options.find(
-                      item => item.value === field.value
-                    );
-
-                    return (
-                      <Form.Item className="flex flex-col flex-1 py-0 w-full">
-                        <Form.Control>
-                          <DropdownMenu>
-                            <div className="flex flex-col gap-y-2 w-full">
-                              <DropdownMenuTrigger
-                                trigger={
-                                  <div className="flex items-center gap-x-2 typo-para-medium text-gray-700">
-                                    {option?.icon && (
-                                      <Icon icon={option.icon} size={'sm'} />
-                                    )}
-                                    {option?.label || ''}
-                                  </div>
-                                }
-                                isExpand
-                                disabled={isDisabled}
-                                className="w-full"
+                  return (
+                    <Form.Item className="flex flex-col flex-1 py-0 w-full">
+                      <Form.Control>
+                        <DropdownMenu>
+                          <div className="flex flex-col gap-y-2 w-full">
+                            <DropdownMenuTrigger
+                              trigger={
+                                <div className="flex items-center gap-x-2 typo-para-medium text-gray-700">
+                                  {option?.icon && (
+                                    <Icon icon={option.icon} size={'sm'} />
+                                  )}
+                                  {option?.label || ''}
+                                </div>
+                              }
+                              isExpand
+                              disabled={isDisabled}
+                              className="w-full"
+                            />
+                          </div>
+                          <DropdownMenuContent align="start">
+                            {options.map((item, index) => (
+                              <DropdownMenuItem
+                                {...field}
+                                key={index}
+                                label={item.label}
+                                value={item.value}
+                                icon={item?.icon}
+                                onSelectOption={() => {
+                                  field.onChange(item.value);
+                                }}
                               />
-                            </div>
-                            <DropdownMenuContent align="start">
-                              {options.map((item, index) => (
-                                <DropdownMenuItem
-                                  {...field}
-                                  key={index}
-                                  label={item.label}
-                                  value={item.value}
-                                  icon={item?.icon}
-                                  onSelectOption={() => {
-                                    field.onChange(item.value);
-                                  }}
-                                />
-                              ))}
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </Form.Control>
-                        <Form.Message />
-                      </Form.Item>
-                    );
-                  }}
-                />
-              </div>
-            </div>
-            <div className="flex items-center w-full gap-x-2 mt-4 typo-para-medium leading-5 text-gray-600 whitespace-nowrap">
-              <Trans
-                i18nKey={
-                  'form:experiments.define-audience.in-experiment-target'
-                }
-                values={{
-                  percent: `${experimentPercentage}%`
-                }}
-                components={{
-                  highlight: (
-                    <div className="flex-center size-fit p-3 rounded-lg typo-para-medium leading-5 text-gray-700 bg-gray-additional-2" />
-                  )
+                            ))}
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </Form.Control>
+                      <Form.Message />
+                    </Form.Item>
+                  );
                 }}
               />
             </div>
           </div>
-          <Divider className="my-5" />
-          <div className="flex items-center gap-x-2">
-            <p className="typo-para-medium text-gray-700">
-              {t('form:experiments.define-audience.split-experiment')}
-            </p>
-            <Tooltip
-              align="start"
-              alignOffset={-68}
-              content={``}
-              trigger={
-                <div className="flex-center size-fit">
-                  <Icon icon={IconInfo} size={'xxs'} color="gray-500" />
-                </div>
-              }
-              className="max-w-[400px]"
+          <div className="flex items-center w-full gap-x-2 mt-4 typo-para-medium leading-5 text-gray-600 whitespace-nowrap">
+            <Trans
+              i18nKey={'form:experiments.define-audience.in-experiment-target'}
+              values={{
+                percent: `${experimentPercentage}%`
+              }}
+              components={{
+                highlight: (
+                  <div className="flex-center size-fit p-3 rounded-lg typo-para-medium leading-5 text-gray-700 bg-gray-additional-2" />
+                )
+              }}
             />
           </div>
+          <Divider className="my-5 border-gray-300" />
+          <p className="typo-para-medium text-gray-700">
+            {t('form:experiments.define-audience.split-experiment-audience')}
+          </p>
           <div className="typo-para-small text-gray-500 mt-3">
             {`${t('form:experiments.define-audience.split-experiment-desc')}`}
           </div>
-
           <RadioGroup
             value={splitOptionType}
             onValueChange={onChangeSplitType}
-            className="flex gap-x-6 mt-5"
+            className="flex gap-x-6 mt-5 px-1"
           >
-            {startOptions.map(({ label, value }) => (
+            {splitExperimentOptions.map(({ label, value }) => (
               <div key={value} className="flex items-center gap-x-2">
                 <RadioGroupItem value={value} id={value} />
                 <label
@@ -442,9 +399,8 @@ const Strategy = ({
               </div>
             ))}
           </RadioGroup>
-          <Divider className="my-5" />
-          <div className="typo-para-small text-gray-500 mt-3">
-            {`Audience slplit`}
+          <div className="typo-para-small text-gray-500 mt-6">
+            {t('form:experiments.define-audience.audience-split')}
           </div>
 
           <Form.Field
@@ -469,11 +425,11 @@ const Strategy = ({
                           )}
                         </div>
                       )}
-                      <div className="flex items-center w-full my-3">
+                      <div className="flex items-center w-full mb-3 mt-5 px-2">
                         <div className="typo-para-small text-gray-500 uppercase flex-1">{`Name`}</div>
                         <div className="typo-para-small text-gray-500 uppercase flex-1">{`Percentage`}</div>
                       </div>
-                      <div className="flex flex-col gap-y-3">
+                      <div className="flex flex-col gap-y-3 px-2">
                         {field.value?.map(
                           (rollout: RuleStrategyVariation, index: number) => (
                             <PercentageInput
