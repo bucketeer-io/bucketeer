9641a42e53a965090194ce345335477e0b1238ef
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-08-18 15:21:10 +0900

    build: update dev container dependencies
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- .github/.devcontainer/local-features/setup-user/install.sh
+++ .github/.devcontainer/local-features/setup-user/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+# Setup user configuration for Bucketeer development environment
 #-------------------------------------------------------------------------------------------------------------
 
 USERNAME=${USERNAME:-"codespace"}
@@ -13,27 +12,29 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+# Ensure that login shells get the correct path
 rm -f /etc/profile.d/00-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" >/etc/profile.d/00-restore-env.sh
+echo "export PATH=/usr/local/go/bin:/go/bin:/usr/local/bin:/home/${USERNAME}/.local/bin:\$PATH" >/etc/profile.d/00-restore-env.sh
+echo "export GOPATH=/go" >>/etc/profile.d/00-restore-env.sh
+echo "export GOROOT=/usr/local/go" >>/etc/profile.d/00-restore-env.sh
 chmod +x /etc/profile.d/00-restore-env.sh
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo_if() {
-  COMMAND="$*"
-  if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
-    su - "$USERNAME" -c "$COMMAND"
-  else
-    "$COMMAND"
-  fi
-}
-
+# Set up user home directory permissions
 HOME_DIR="/home/${USERNAME}/"
-chown -R ${USERNAME}:${USERNAME} ${HOME_DIR}
-chmod -R g+r+w "${HOME_DIR}"
-find "${HOME_DIR}" -type d | xargs -n 1 chmod g+s
+if [ -d "${HOME_DIR}" ]; then
+  chown -R ${USERNAME}:${USERNAME} ${HOME_DIR}
+  chmod -R g+r+w "${HOME_DIR}"
+  find "${HOME_DIR}" -type d | xargs -n 1 chmod g+s
+fi
 
-echo "Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/share:/home/${USERNAME}/.local/bin:${PATH}\"" >>/etc/sudoers.d/$USERNAME
+# Create npm global directory for user
+NPM_GLOBAL_DIR="/home/${USERNAME}/.npm-global"
+mkdir -p "${NPM_GLOBAL_DIR}"
+chown -R ${USERNAME}:${USERNAME} "${NPM_GLOBAL_DIR}"
 
-echo "Done!"
+# Configure sudo PATH
+echo "Defaults secure_path=\"/usr/local/go/bin:/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/${USERNAME}/.local/bin\"" >>/etc/sudoers.d/$USERNAME
+
+echo "User setup complete!"
