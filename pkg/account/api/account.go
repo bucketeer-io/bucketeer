8fc2a11ffe9b61041cee3d7db19f50fec27d83c4
Cyka <1354250064mdzz@gmail.com>
2025-08-25 18:25:29 +0800

    refactor(auth): add credentials record creation for password setup process

--- pkg/account/api/account.go
+++ pkg/account/api/account.go
@@ -24,6 +24,8 @@ import (
 	"github.com/jinzhu/copier"
 	"go.uber.org/zap"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/bucketeer-io/bucketeer/pkg/account/command"
@@ -1410,6 +1412,12 @@ func (s *AccountService) initiatePasswordSetupForNewAccount(ctx context.Context,
 		Email: email,
 	})
 	if err != nil {
+		// Check if the error is because password already exists
+		if st, ok := status.FromError(err); ok && st.Code() == codes.AlreadyExists {
+			s.logger.Info("Password already exists for account, skipping setup", zap.String("email", email))
+			return
+		}
+
 		s.logger.Warn("Failed to initiate password setup for new account",
 			zap.Error(err),
 			zap.String("email", email),
