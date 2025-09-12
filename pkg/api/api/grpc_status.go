bb7a54c7f93951d8f945607610884c6324125239
kakcy <yokeyphone@gmail.com>
2025-09-12 16:32:04 +0900

    reformat

--- pkg/api/api/grpc_status.go
+++ pkg/api/api/grpc_status.go
@@ -120,8 +120,6 @@ func convertErrorReason(errorType pkgErr.ErrorType) string {
 		return "UNEXPECTED_AFFECTED_ROWS"
 	case pkgErr.ErrorTypeInternal:
 		return "INTERNAL"
-	case pkgErr.ErrorTypeFailedPrecondition:
-		return "FAILED_PRECONDITION"
 	default:
 		return "UNKNOWN"
 	}
@@ -149,8 +147,6 @@ func convertStatusCode(errorType pkgErr.ErrorType) codes.Code {
 		return codes.Internal
 	case pkgErr.ErrorTypeInternal:
 		return codes.Internal
-	case pkgErr.ErrorTypeFailedPrecondition:
-		return codes.FailedPrecondition
 	default:
 		return codes.Unknown
 	}
