1787c852496410d53ba712fd44773efd4a3fba06
Cyka <1354250064mdzz@gmail.com>
2025-09-23 10:08:07 +0800

    feat(auth): implement authentication settings for organizations

--- pkg/auth/api/api.go
+++ pkg/auth/api/api.go
@@ -768,6 +768,12 @@ func (s *authService) PrepareDemoUser() {
 						CreatedAt:   now.Unix(),
 						UpdatedAt:   now.Unix(),
 						SystemAdmin: config.IsSystemAdmin,
+						AuthenticationSettings: &envproto.AuthenticationSettings{
+							EnabledTypes: []envproto.AuthenticationType{
+								envproto.AuthenticationType_AUTHENTICATION_TYPE_GOOGLE,
+								envproto.AuthenticationType_AUTHENTICATION_TYPE_PASSWORD,
+							},
+						},
 					},
 				})
 			}
