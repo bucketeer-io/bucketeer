412f09ac7afcd4ec807914d6781d06cfaaa43fe4
Cyka <1354250064mdzz@gmail.com>
2025-09-26 13:16:41 +0800

    chore(auth): update import paths to use v2 package structure

--- pkg/auth/storage/mock/storage.go
+++ pkg/auth/storage/mock/storage.go
@@ -15,7 +15,7 @@ import (
 
 	gomock "go.uber.org/mock/gomock"
 
-	domain "github.com/bucketeer-io/bucketeer/pkg/auth/domain"
+	domain "github.com/bucketeer-io/bucketeer/v2/pkg/auth/domain"
 )
 
 // MockCredentialsStorage is a mock of CredentialsStorage interface.
