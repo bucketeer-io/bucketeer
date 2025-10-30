66b824224a5cdaf8b878f757446d5251bc7f16c0
Cyka <1354250064mdzz@gmail.com>
2025-10-30 12:35:29 +0800

    test(auth): add helper function for default authentication settings in tests

--- pkg/environment/api/organization_test.go
+++ pkg/environment/api/organization_test.go
@@ -30,6 +30,15 @@ import (
 	proto "github.com/bucketeer-io/bucketeer/v2/proto/environment"
 )
 
+// Helper function to create default authentication settings for tests
+func newTestAuthSettings() *proto.AuthenticationSettings {
+	return &proto.AuthenticationSettings{
+		EnabledTypes: []proto.AuthenticationType{
+			proto.AuthenticationType_AUTHENTICATION_TYPE_GOOGLE,
+		},
+	}
+}
+
 func TestGetOrganizationMySQL(t *testing.T) {
 	t.Parallel()
 	mockController := gomock.NewController(t)
@@ -205,7 +214,7 @@ func TestCreateOrganizationMySQL(t *testing.T) {
 		"description",
 		false,
 		false,
-		true,
+		newTestAuthSettings(),
 	)
 	require.NoError(t, err)
 	trialOrgExpected, err := domain.NewOrganization(
@@ -215,7 +224,7 @@ func TestCreateOrganizationMySQL(t *testing.T) {
 		"description2",
 		true,
 		false,
-		true,
+		newTestAuthSettings(),
 	)
 	require.NoError(t, err)
 
@@ -1158,7 +1167,7 @@ func TestEnvironmentService_CreateDemoOrganization(t *testing.T) {
 		"description",
 		false,
 		false,
-		true,
+		newTestAuthSettings(),
 	)
 	require.NoError(t, err)
 
