c028bb9b1674c2d8724a4f6e9c93ba69b4fb9425
steveninhle <lephuocninh7118@gmail.com>
2025-11-07 16:42:16 +0700

    fix:event counnt not show

--- ui/dashboard/src/pages/feature-flag-details/evaluation/utils.ts
+++ ui/dashboard/src/pages/feature-flag-details/evaluation/utils.ts
@@ -14,8 +14,8 @@ export const symlogInverse = (value: number, c = 1, base = 10): number => {
   return Math.sign(value) * ((Math.pow(base, Math.abs(value)) - 1) * c);
 };
 
-export const fomatLabel = (value: number): string => {
-  const abs = Math.abs(value);
+export const formatLabel = (value: number): string => {
+  const abs = Math.round(Math.abs(value));
   if (abs >= 1e9) return `${Math.round(value / 1e9)}B`;
   if (abs >= 1e6) return `${Math.round(value / 1e6)}M`;
   if (abs >= 1e3) return `${Math.round(value / 1e3)}K`;
