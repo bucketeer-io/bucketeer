011a411c32d958503481b016604d78b6d983610f
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2026-01-29 11:45:12 +0900

    refactor: minor change
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- pkg/batch/jobs/opsevent/progressove_rollout_watcher.go
+++ pkg/batch/jobs/opsevent/progressove_rollout_watcher.go
@@ -82,10 +82,10 @@ func (w *progressiveRolloutWatcher) Run(
 		}
 		var executed bool
 		for _, p := range progressiveRollouts {
-			pr := &autoopsdomain.ProgressiveRollout{ProgressiveRollout: p}
+			progressiveRollout := &autoopsdomain.ProgressiveRollout{ProgressiveRollout: p}
 			// Skip finished or stopped progressive rollouts to avoid unnecessary processing
 			// This is consistent with datetime_watcher and event_count_watcher behavior
-			if pr.IsFinished() || pr.IsStopped() {
+			if progressiveRollout.IsFinished() || progressiveRollout.IsStopped() {
 				continue
 			}
 			wasExecuted, err := w.executeProgressiveRollout(ctx, p, e.Id)
