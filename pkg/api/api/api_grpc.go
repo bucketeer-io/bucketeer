94005529c2d5d8521490f0849d8c0b71c85199c9
t-kikuc <tkikuchi07f@gmail.com>
2026-02-19 19:14:32 +0900

    add comment in RegisterEvents
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/api/api/api_grpc.go
+++ pkg/api/api/api_grpc.go
@@ -1439,6 +1439,8 @@ func (s *grpcGatewayService) RegisterEvents(
 				continue
 			}
 
+			// Record DAU from RegisterEvents as well because
+			// local evaluations do not include userId in GetEvaluation(s) requests.
 			go func(evt *eventproto.Event, envID string) {
 				evalEvent := &eventproto.EvaluationEvent{}
 				if err := evt.Event.UnmarshalTo(evalEvent); err != nil {
