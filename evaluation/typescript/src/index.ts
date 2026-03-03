d20daa90ea3e437056c4ac3dd1b1de6eba618180
duyhungtnn <duyhung71089@gmail.com>
2026-03-03 14:00:27 +0700

    chore: add better filter proto file command

--- evaluation/typescript/src/index.ts
+++ evaluation/typescript/src/index.ts
@@ -4,7 +4,7 @@ import { Feature } from './proto/feature/feature_pb';
 import { NewUserEvaluations } from './userEvaluation';
 import { User } from './proto/user/user_pb';
 import { Evaluation, UserEvaluations } from './proto/feature/evaluation_pb';
-import { Strategy } from './proto/feature/strategy_pb';
+import { Strategy, RolloutStrategy, FixedStrategy } from './proto/feature/strategy_pb';
 import { Clause } from './proto/feature/clause_pb';
 import { Reason } from './proto/feature/reason_pb';
 import { Rule } from './proto/feature/rule_pb';
@@ -13,7 +13,6 @@ import { Variation } from './proto/feature/variation_pb';
 import { Prerequisite } from './proto/feature/prerequisite_pb';
 import { FeatureLastUsedInfo } from './proto/feature/feature_last_used_info_pb';
 import { SourceId } from './proto/event/client/event_pb';
-import { RolloutStrategy, FixedStrategy } from './proto/feature/strategy_pb';
 import {
   createClause,
   createFixedStrategy,
