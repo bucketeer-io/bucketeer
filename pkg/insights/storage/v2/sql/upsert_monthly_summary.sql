703a26f49f5330e3613dab29847053494fe248c0
t-kikuc <tkikuchi07f@gmail.com>
2026-03-06 19:49:12 +0900

    add monthly_summary storage
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

---
+++ pkg/insights/storage/v2/sql/upsert_monthly_summary.sql
@@ -0,0 +1,13 @@
+INSERT INTO monthly_summary (
+    environment_id,
+    source_id,
+    yearmonth,
+    mau,
+    request_count,
+    created_at,
+    updated_at
+) VALUES %s
+ON DUPLICATE KEY UPDATE
+    mau = VALUES(mau),
+    request_count = VALUES(request_count),
+    updated_at = VALUES(updated_at)
