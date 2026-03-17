4808e9f9f8cbcc348e97fae7ecb1cfce0602c313
steveninhle <lephuocninh7118@gmail.com>
2026-03-17 09:33:52 +0700

    handle conflic with main branch

--- ui/dashboard/src/hooks/use-screen.ts
+++ ui/dashboard/src/hooks/use-screen.ts
@@ -1,7 +1,8 @@
 import { useMediaQuery } from 'react-responsive';
 
 export const useScreen = () => {
-  const fromTabletScreen = useMediaQuery({ minWidth: 768 });
+  const fromMobileScreen = useMediaQuery({ minWidth: 600 });
+  const fromTabletScreen = useMediaQuery({ minWidth: 900 });
   const fromXLScreen = useMediaQuery({ minWidth: 1440 });
   const from2XLScreen = useMediaQuery({ minWidth: 1600 });
   const from3XLScreen = useMediaQuery({ minWidth: 1920 });
@@ -9,12 +10,12 @@ export const useScreen = () => {
   const lessThanXLScreen = useMediaQuery({ maxWidth: 1439 });
 
   return {
+    fromMobileScreen,
     fromTabletScreen,
     fromXLScreen,
     from2XLScreen,
     from3XLScreen,
     from4XLScreen,
-
     lessThanXLScreen
   };
 };
