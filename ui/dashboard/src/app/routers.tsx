1f52a3f01f839af9914424fd9e33ab8c8f304355
steveninhle <lephuocninh7118@gmail.com>
2026-09-22 09:41:40 +0700

    chore(ui/dashboard): convert route tables to RouteObject arrays

--- ui/dashboard/src/app/routers.tsx
+++ ui/dashboard/src/app/routers.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { RouteObject, useRoutes } from 'react-router';
 import { ID_CLONE, ID_NEW } from 'constants/routing';
 import CreateFlagPage from 'pages/create-flag';
 import CreateUpdateSegmentPage from 'pages/create-update-segment';
@@ -15,76 +15,56 @@ import ProjectDetailsPage from 'pages/project-details';
 import ProjectsPage from 'pages/projects';
 import UserSegmentsPage from 'pages/user-segments';
 
-export const OrganizationsRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<OrganizationsPage />} />
-      <Route path="new" element={<OrganizationsPage />} />
-      <Route path=":organizationId" element={<OrganizationsPage />} />
-      <Route path=":organizationId/*" element={<OrganizationDetailPage />} />
-    </Routes>
-  );
-};
+export const organizationsRoutes: RouteObject[] = [
+  { index: true, element: <OrganizationsPage /> },
+  { path: 'new', element: <OrganizationsPage /> },
+  { path: ':organizationId', element: <OrganizationsPage /> },
+  { path: ':organizationId/*', element: <OrganizationDetailPage /> }
+];
 
-export const ProjectsRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<ProjectsPage />} />
-      <Route path="new" element={<ProjectsPage />} />
-      <Route path=":projectId" element={<ProjectsPage />} />
-      <Route path=":projectId/*" element={<ProjectDetailsPage />} />
-    </Routes>
-  );
-};
+export const projectsRoutes: RouteObject[] = [
+  { index: true, element: <ProjectsPage /> },
+  { path: 'new', element: <ProjectsPage /> },
+  { path: ':projectId', element: <ProjectsPage /> },
+  { path: ':projectId/*', element: <ProjectDetailsPage /> }
+];
 
-export const ExperimentsRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<ExperimentsPage />} />
-      <Route path="new" element={<ExperimentsPage />} />
-      <Route path=":experimentId" element={<ExperimentsPage />} />
-      <Route path=":experimentId/:tab" element={<ExperimentDetailsPage />} />
-    </Routes>
-  );
-};
+export const experimentsRoutes: RouteObject[] = [
+  { index: true, element: <ExperimentsPage /> },
+  { path: 'new', element: <ExperimentsPage /> },
+  { path: ':experimentId', element: <ExperimentsPage /> },
+  { path: ':experimentId/:tab', element: <ExperimentDetailsPage /> }
+];
 
-export const GoalsRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<GoalsPage />} />
-      <Route path="new" element={<GoalsPage />} />
-      <Route path=":goalId/*" element={<GoalDetailsPage />} />
-    </Routes>
-  );
-};
+export const goalsRoutes: RouteObject[] = [
+  { index: true, element: <GoalsPage /> },
+  { path: 'new', element: <GoalsPage /> },
+  { path: ':goalId/*', element: <GoalDetailsPage /> }
+];
 
-export const FeatureFlagsRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<FeatureFlagsPage />} />
-      <Route path={ID_NEW} element={<CreateFlagPage />} />
-      <Route path={`${ID_CLONE}/:flagId`} element={<FeatureFlagsPage />} />
-      <Route path=":flagId/*" element={<FeatureFlagDetailsPage />} />
-    </Routes>
-  );
-};
+export const featureFlagsRoutes: RouteObject[] = [
+  { index: true, element: <FeatureFlagsPage /> },
+  { path: ID_NEW, element: <CreateFlagPage /> },
+  { path: `${ID_CLONE}/:flagId`, element: <FeatureFlagsPage /> },
+  { path: ':flagId/*', element: <FeatureFlagDetailsPage /> }
+];
 
-export const UserSegmentsRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<UserSegmentsPage />} />
-      <Route path={ID_NEW} element={<CreateUpdateSegmentPage />} />
-      <Route path=":segmentId" element={<CreateUpdateSegmentPage />} />
-    </Routes>
-  );
-};
+export const userSegmentsRoutes: RouteObject[] = [
+  { index: true, element: <UserSegmentsPage /> },
+  { path: ID_NEW, element: <CreateUpdateSegmentPage /> },
+  { path: ':segmentId', element: <CreateUpdateSegmentPage /> }
+];
 
-export const MemberRoot = () => {
-  return (
-    <Routes>
-      <Route index element={<MembersPage />} />
-      <Route path={ID_NEW} element={<MembersPage />} />
-      <Route path=":memberId" element={<MembersPage />} />
-    </Routes>
-  );
-};
+export const memberRoutes: RouteObject[] = [
+  { index: true, element: <MembersPage /> },
+  { path: ID_NEW, element: <MembersPage /> },
+  { path: ':memberId', element: <MembersPage /> }
+];
+
+export const OrganizationsRoot = () => useRoutes(organizationsRoutes);
+export const ProjectsRoot = () => useRoutes(projectsRoutes);
+export const ExperimentsRoot = () => useRoutes(experimentsRoutes);
+export const GoalsRoot = () => useRoutes(goalsRoutes);
+export const FeatureFlagsRoot = () => useRoutes(featureFlagsRoutes);
+export const UserSegmentsRoot = () => useRoutes(userSegmentsRoutes);
+export const MemberRoot = () => useRoutes(memberRoutes);
